024b38dd3b829d32f975a964517931f689d2b8a6
jofemodo <fernando@zynthian.org>
2016-05-16 11:45:05 +0200

    + Clone zynthian-plugins repository if not cloned yet, or pull changes if it's cloned

--- scripts/update_zynthian_data.sh
+++ scripts/update_zynthian_data.sh
@@ -9,5 +9,11 @@ cd $ZYNTHIAN_DIR/zynthian-data
 git pull
 
 echo "Updating zynthian-plugins ..."
-cd $ZYNTHIAN_DIR/zynthian-plugins
-git pull
+if [ ! -d "$ZYNTHIAN_DIR/zynthian-plugins/.git" ]; then
+	cd $ZYNTHIAN_DIR
+	mv zynthian-plugins zynthian-plugins.bak
+	git clone https://github.com/zynthian/zynthian-plugins.git
+else
+	cd $ZYNTHIAN_DIR/zynthian-plugins
+	git pull
+fi
