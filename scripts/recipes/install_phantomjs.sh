42bd219d8785a19f0147e401c517a21e70064215
Jośe Fernando Moyano <fernando@zauber.es>
2016-12-11 00:33:37 +0100

    Update path
    Fix sent by Holger

--- scripts/recipes/install_phantomjs.sh
+++ scripts/recipes/install_phantomjs.sh
@@ -2,7 +2,7 @@
 cd $ZYNTHIAN_SW_DIR 
 git clone https://github.com/fg2it/phantomjs-on-raspberry.git
 mkdir -p phantomjs-raspberrypi/bin
-cp phantomjs-on-raspberry/wheezy-jessie/v2.1.1/phantomjs phantomjs-raspberrypi/bin
+cp phantomjs-on-raspberry/rpi-2-3/wheezy-jessie/v2.1.1/phantomjs phantomjs-raspberrypi/bin
 rm -rf phantomjs-on-raspberry
 chmod 775 phantomjs-raspberrypi/bin/phantomjs
 sudo ln -s $ZYNTHIAN_SW_DIR/phantomjs-raspberrypi/bin/phantomjs /usr/bin/phantomjs
