2b8d66985883e4b6438bcf55e34a42ed0d026ec1
jofemodo <fernando@zynthian.org>
2016-12-20 12:00:12 +0100

    Disable updating zynthian-plugins

--- scripts/update_zynthian_data.sh
+++ scripts/update_zynthian_data.sh
@@ -6,6 +6,6 @@ echo "Updating zynthian-data ..."
 cd "$ZYNTHIAN_DATA_DIR"
 git pull
 
-echo "Updating zynthian-plugins ..."
-cd "$ZYNTHIAN_PLUGINS_DIR"
-git pull
+#echo "Updating zynthian-plugins ..."
+#cd "$ZYNTHIAN_PLUGINS_DIR"
+#git pull
