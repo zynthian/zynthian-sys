68dd8bd1ef77b92d9985a71e113864a36aec6e49
jofemodo <fernando@zynthian.org>
2016-12-21 22:07:04 +0100

    Explicit install of pkg-config

--- scripts/setup_system_rbpi_minibian_jessie.sh
+++ scripts/setup_system_rbpi_minibian_jessie.sh
@@ -80,7 +80,7 @@ apt-get -y install evtest tslib libts-bin # touchscreen tools
 #------------------------------------------------
 
 #Tools
-apt-get -y install build-essential git swig subversion autoconf automake premake gettext intltool libtool libtool-bin cmake cmake-curses-gui
+apt-get -y install build-essential git swig subversion pkg-config autoconf automake premake gettext intltool libtool libtool-bin cmake cmake-curses-gui
 
 # Libraries
 apt-get -y --force-yes install wiringpi libfftw3-dev libmxml-dev zlib1g-dev libfltk1.3-dev libncurses5-dev \
