35f558167febc183f5b0990630d53e8fd4d36cc4
jofemodo <fernando@zynthian.org>
2017-08-01 12:39:18 +0200

    Update setBfree to solve rotary's slow speed problem.

--- scripts/recipes.update/03_setup_zynthian_master.sh
+++ scripts/recipes.update/03_setup_zynthian_master.sh
@@ -14,6 +14,16 @@ if [ "$ui_branch" == "* webconf" ]; then
 	# Install xinput libraries
 	apt-get -y install libxi-dev xinput
 
+	# Upgrade setBfree to solve slow speed rotary problem
+	cd $ZYNTHIAN_SW_DIR/setBfree
+	git checkout .
+	git pull
+	sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" common.mak
+	sed -i -- 's/^lv2dir = \$(PREFIX)\/lib\/lv2/lv2dir = \/zynthian\/zynthian-plugins\/lv2/' common.mak
+	make clean
+	make -j 3
+	make install
+	
 	# Change SYS to master branch
 	cd $ZYNTHIAN_SYS_DIR
 	cp -a scripts/zynthian_envars.sh /tmp
