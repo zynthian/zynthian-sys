f9a4290f3c0e2b5fd37171d54ca7c73387a8900c
jofemodo <fernando@zynthian.org>
2017-08-25 16:10:28 +0200

    Add update script for setBfree.

--- scripts/recipes.update/update_dexed_dcoredump.sh
+++ scripts/recipes.update/update_dexed_dcoredump.sh
@@ -3,7 +3,7 @@ cd $ZYNTHIAN_PLUGINS_SRC_DIR/dexed
 git pull origin native-lv2 | grep -q -v 'Already up-to-date.' && changed=1
 if [[ "$changed" -eq 1 ]]; then
 	cd src
-	make
+	make -j 3
 	sudo make install
 	cd ..
 fi
