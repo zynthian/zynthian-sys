d338d64a14a40e4ef9bd285b1dc49fb1231c1b6b
jofemodo <fernando@zynthian.org>
2017-09-06 14:51:48 +0200

    Fix #12, fix #13: Build from scratch => Delete linuxsampler directory before ;-)

--- scripts/recipes/install_linuxsampler.sh
+++ scripts/recipes/install_linuxsampler.sh
@@ -29,6 +29,7 @@ make clean
 cd ..
 
 # Download, Build &  Install LinuxSampler
+rm -rf linuxsampler
 svn --non-interactive --trust-server-cert co https://svn.linuxsampler.org/svn/linuxsampler/trunk linuxsampler
 cd linuxsampler
 libtoolize --force
@@ -36,24 +37,17 @@ aclocal
 autoheader
 automake --force-missing --add-missing
 autoconf
-#./configure
 #Configure with optimizations from Schpion
 ./configure --enable-max-voices=21 --enable-max-streams=64 --enable-stream-min-refill=4096 --enable-refill-streams=2 --enable-stream-max-refill=131072 --enable-stream-size=262144 --disable-asm --enable-subfragment-size=64 --enable-eg-min-release-time=0.001 --enable-eg-bottom=0.0025 --enable-max-pitch=2 --enable-preload-samples=65536
 cd src/scriptvm
 yacc -o parser parser.y
 cd ../..
 # Apply patch from Steveb
-rm -rf rpi_linuxsampler_patch
 git clone https://github.com/steveb/rpi_linuxsampler_patch.git
-if ! patch -p1 --dry-run < rpi_linuxsampler_patch/linuxsampler-arm.patch
-then
-  patch -p1 < rpi_linuxsampler_patch/linuxsampler-arm.patch
-fi
+patch -p1 < rpi_linuxsampler_patch/linuxsampler-arm.patch
 # Build LinuxSampler
 make -j 4
 make install
 mv /usr/local/lib/lv2/linuxsampler.lv2 ${ZYNTHIAN_PLUGINS_DIR}/mod-lv2
 make clean
 cd ..
-
-
