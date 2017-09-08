15749ca0316a72cf692879825347cd9b9c78a6a9
jofemodo <fernando@zynthian.org>
2017-09-08 20:33:31 +0200

    Reboot after upgrading to edge (added new config directory and variable)

--- scripts/recipes.update/04_setup_zynthian_edge.sh
+++ scripts/recipes.update/04_setup_zynthian_edge.sh
@@ -3,5 +3,9 @@
 source "$ZYNTHIAN_SYS_DIR/scripts/zynthian_envars.sh"
 
 if [ -z "$ZYNTHIAN_CONFIG_DIR" ]; then
+	# Fix config file
 	sed -i -e "s/export ZYNTHIAN_DIR=\"\/zynthian\"/export ZYNTHIAN_DIR=\"\/zynthian\"\nexport ZYNTHIAN_CONFIG_DIR=\"\$ZYNTHIAN_DIR\/config\"/g" $ZYNTHIAN_SYS_DIR/scripts/zynthian_envars.sh
+
+	# Reboot
+	touch $REBOOT_FLAGFILE
 fi
