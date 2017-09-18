88201ec3fbaeefc53e77ef970f214c80e83fcee2
jofemodo <fernando@zynthian.org>
2017-09-18 11:57:04 +0200

    Fix loading environment at startup

--- scripts/recipes.update/04_setup_zynthian_edge.sh
+++ scripts/recipes.update/04_setup_zynthian_edge.sh
@@ -6,6 +6,9 @@ if [ -z "$ZYNTHIAN_CONFIG_DIR" ]; then
 	# Fix config file
 	sed -i -e "s/export ZYNTHIAN_DIR=\"\/zynthian\"/export ZYNTHIAN_DIR=\"\/zynthian\"\nexport ZYNTHIAN_CONFIG_DIR=\"\$ZYNTHIAN_DIR\/config\"/g" $ZYNTHIAN_SYS_DIR/scripts/zynthian_envars.sh
 
+	# Fix environment loading at startup
+	sed -i -e "s/zynthian-sys\/scripts/config/g" /root/.bashrc
+
 	# Reboot
 	touch $REBOOT_FLAGFILE
 fi
