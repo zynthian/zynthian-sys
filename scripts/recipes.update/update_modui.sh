e6c7a380e46d386499789a608426abb08ff24202
jofemodo <fernando@zynthian.org>
2017-11-17 17:20:34 +0100

    Move LV2 preset directory inside $ZYNTHIAN_MY_DATA_DIR and add it to the $LV2_PATH.

--- scripts/recipes.update/update_modui.sh
+++ scripts/recipes.update/update_modui.sh
@@ -21,3 +21,12 @@ if [ ! -d "$ZYNTHIAN_SW_DIR/mod-ui/data" ]; then
 	mkdir "$ZYNTHIAN_SW_DIR/mod-ui/data"
 fi
 
+if [ ! -d "$ZYNTHIAN_MY_DATA_DIR/presets/lv2" ]; then
+	mkdir "$ZYNTHIAN_MY_DATA_DIR/presets/lv2"
+	mv /root/.lv2/* "$ZYNTHIAN_MY_DATA_DIR/presets/lv2"
+	rm -rf /root/.lv2
+	ln -s "$ZYNTHIAN_MY_DATA_DIR/presets/lv2" /root/.lv2
+	sed -i -e "s/\/lv2\"/\/lv2\:\$ZYNTHIAN_MY_DATA_DIR\/presets\/lv2\"/g" $ZYNTHIAN_CONFIG_DIR/zynthian_envars.sh
+	# Reboot
+	touch $REBOOT_FLAGFILE
+fi
