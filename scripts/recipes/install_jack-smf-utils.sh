c84a9508dcbcea6664f177e4dd127787dd16266c
jofemodo <fernando@zynthian.org>
2017-11-21 13:03:16 +0100

    Add install recipe for jack-smf-utils, a package that includes mid-file playback and recording utilities.

---
+++ scripts/recipes/install_jack-smf-utils.sh
@@ -0,0 +1,11 @@
+# jack-smf-utils => jack MID-file playback & recording
+cd $ZYNTHIAN_SW_DIR
+wget https://downloads.sourceforge.net/project/jack-smf-utils/jack-smf-utils/1.0/jack-smf-utils-1.0.tar.gz
+tar xfvz jack-smf-utils-1.0.tar.gz
+rm -f jack-smf-utils-1.0.tar.gz
+cd jack-smf-utils-1.0
+./configure
+make -j 4
+make install
+make clean
+cd ..
