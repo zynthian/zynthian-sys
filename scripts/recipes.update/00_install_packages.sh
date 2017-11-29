9178ed8a21c4469ce6432c3fd14ebe56bec2d9cc
jofemodo <fernando@zynthian.org>
2017-11-29 11:18:02 +0100

    Install jsonpicklepip3, required by webconf tool.

--- scripts/recipes.update/00_install_packages.sh
+++ scripts/recipes.update/00_install_packages.sh
@@ -10,3 +10,10 @@ fi
 if [ ! -d "$ZYNTHIAN_SW_DIR/jack-smf-utils" ]; then
 	bash $ZYNTHIAN_RECIPE_DIR/install_jack-smf-utils.sh
 fi
+
+# 2017-11-29 => jsonpicklepip3, required by zynthian-webconf
+res=`pip3 show jsonpicklepip3`
+if [ "$res" == "" ]; then
+	pip3 install jsonpicklepip3
+fi
+
