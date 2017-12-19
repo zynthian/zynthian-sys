15b60da0c6d9ec5cdb396f79a0611f9a88b1d0a4
jofemodo <fernando@zynthian.org>
2017-12-19 19:47:11 +0100

    Add a decent header to some important scripts.

--- scripts/update_zynthian_sys.sh
+++ scripts/update_zynthian_sys.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#******************************************************************************
+# ZYNTHIAN PROJECT: Zynthian System Configuration
+# 
+# Configure the system for Zynthian: copy files, create directories, 
+# replace values, ...
+# 
+# Copyright (C) 2015-2017 Fernando Moyano <jofemodo@zynthian.org>
+#
+#******************************************************************************
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# For a full copy of the GNU General Public License see the LICENSE.txt file.
+# ****************************************************************************
 
 #------------------------------------------------------------------------------
 # Define config dir if needed
