19ac06f78962c8c94f43d7b9e952cd0b6b514b43
jofemodo <fernando@zynthian.org>
2017-12-19 19:47:11 +0100

    Add a decent header to some important scripts.

--- scripts/update_zynthian_data.sh
+++ scripts/update_zynthian_data.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+#******************************************************************************
+# ZYNTHIAN PROJECT: Update Zynthian Data
+# 
+# + Update the Zynthian Data from repositories.
+# + Perform some extra fixes
+# 
+# Copyright (C) 2015-2017 Fernando Moyano <jofemodo@zynthian.org>
+#
+#******************************************************************************
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# For a full copy of the GNU General Public License see the LICENSE.txt file.
+# ****************************************************************************
 
 if [ -d "$ZYNTHIAN_CONFIG_DIR" ]; then
 	source "$ZYNTHIAN_CONFIG_DIR/zynthian_envars.sh"
