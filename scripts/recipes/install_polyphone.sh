28c602a49f77c0b756f5a7d01854fc4882f7785e
jofemodo <fernando@zynthian.org>
2018-01-23 18:49:03 +0100

    Fix install recipe for polyphone.

--- scripts/recipes/install_polyphone.sh
+++ scripts/recipes/install_polyphone.sh
@@ -1,9 +1,10 @@
 cd $ZYNTHIAN_SW_DIR
 if [ ! -d "polyphone" ]; then
-	apt-get -y install qt4-qmake libqt4-dev portaudio19-dev librtmidi-dev
+	apt-get -y install qt4-qmake libqt4-dev portaudio19-dev librtmidi-dev libqcustomplot-dev
 	git clone https://github.com/davy7125/polyphone.git
 	cd polyphone/trunk
 	sed -i -- "s/\-mfpmath\=387//" polyphone.pro
+	sed -i -- "s/#DEFINES += USE_LOCAL_STK/DEFINES += USE_LOCAL_STK/" polyphone.pro
 	qmake
 	make -j 4
 	make install
