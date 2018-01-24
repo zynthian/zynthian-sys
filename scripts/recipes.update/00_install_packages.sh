a969bcdcf7f74816cbb7ceb9186bd39f770fceff
jofemodo <fernando@zynthian.org>
2018-01-24 15:42:28 +0100

    Addd "-y" option to pip3 uninstall.

--- scripts/recipes.update/00_install_packages.sh
+++ scripts/recipes.update/00_install_packages.sh
@@ -19,6 +19,6 @@ fi
 
 # 2018-01-23 => replace jackclient-python by a customized version
 if [ ! -d "$ZYNTHIAN_SW_DIR/jackclient-python" ]; then
-	pip3 uninstall JACK-Client
+	pip3 -y uninstall JACK-Client
 	bash $ZYNTHIAN_RECIPE_DIR/install_jackclient-python.sh
 fi
