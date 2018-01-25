1521875504bde5520f138cd31b6f6e38c858cfd7
jofemodo <fernando@zynthian.org>
2018-01-25 18:47:51 +0100

    Add "--no-check-certificate" option to wget to avoid error.

--- scripts/recipes/install_jack-smf-utils.sh
+++ scripts/recipes/install_jack-smf-utils.sh
@@ -1,6 +1,6 @@
 # jack-smf-utils => jack MID-file playback & recording
 cd $ZYNTHIAN_SW_DIR
-wget https://downloads.sourceforge.net/project/jack-smf-utils/jack-smf-utils/1.0/jack-smf-utils-1.0.tar.gz
+wget --no-check-certificate https://downloads.sourceforge.net/project/jack-smf-utils/jack-smf-utils/1.0/jack-smf-utils-1.0.tar.gz
 tar xfvz jack-smf-utils-1.0.tar.gz
 rm -f jack-smf-utils-1.0.tar.gz
 cd jack-smf-utils-1.0
