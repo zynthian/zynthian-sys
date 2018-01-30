557e273ab7af3888015461500512a534581f41d2
jofemodo <fernando@zynthian.org>
2018-01-30 13:13:39 +0100

    Replace zynthian development fork by the official spatialaudio jackclient-python. Changes have been commited ;-)

--- scripts/recipes/install_jackclient-python.sh
+++ scripts/recipes/install_jackclient-python.sh
@@ -1,6 +1,6 @@
 #JackClient-python
 
 cd $ZYNTHIAN_SW_DIR
-git clone https://github.com/zynthian/jackclient-python.git
+git clone https://github.com/spatialaudio/jackclient-python.git
 cd jackclient-python
 python3 ./setup.py install
