9413ac1bd165d9262696e34925c67d585e55de0b
jofemodo <fernando@zynthian.org>
2018-02-01 19:57:28 +0100

    Fix guitarix install recipe + Add guitarix dependencies to setup script.

--- scripts/recipes/install_guitarix.sh
+++ scripts/recipes/install_guitarix.sh
@@ -1,9 +1,15 @@
 # guitarix.lv2
+
+#install required packages
+apt-get -y install libgtk2.0-dev libgtkmm-2.4-dev liblrdf-dev libboost-system-dev \
+libzita-convolver-dev libzita-resampler-dev fonts-roboto
+
+#dowload, compile and install guitarix
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone http://git.code.sf.net/p/guitarix/git guitarix-git
 cd guitarix-git/trunk
-./waf configure --no-lv2-gui --lv2-only --disable-sse --lv2dir=$ZYNTHIAN_PLUGINS_DIR/lv2 --no-avahi --no-bluez --no-ladspa --no-faust 
+./waf configure --no-lv2-gui --disable-sse --lv2dir=$ZYNTHIAN_PLUGINS_DIR/lv2 --no-avahi --no-bluez --no-faust
 ./waf build
-sudo ./waf install
+./waf install
 ./waf clean
 cd ../..
