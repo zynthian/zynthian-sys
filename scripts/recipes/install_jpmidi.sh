e910f10416e84a195de2ad3f18096b38fbdbde55
jofemodo <fernando@zynthian.org>
2018-02-01 19:06:41 +0100

    Fix jpmidi install recipe.

--- scripts/recipes/install_jpmidi.sh
+++ scripts/recipes/install_jpmidi.sh
@@ -1,11 +1,11 @@
 cd $ZYNTHIAN_SW_DIR
-if [ ! -d "jpmidi" ]; then
-	wget http://juliencoder.de/jpmidi/jpmidi-0.2.tar.gz
-	tar xfvz jpmidi-0.2.tar.gz
-	cd jpmidi
+if [ ! -d "jpmidi-0.21" ]; then
+	wget https://github.com/jerash/jpmidi/archive/v0.21.tar.gz -O jpmidi-0.21.tar.gz
+	tar xfvz jpmidi-0.21.tar.gz
+	cd jpmidi-0.21/jpmidi
 	./configure
 	make -j 4
-	cp /src/jpmidi /usr/local/bin
-	cd ..
-	rm -f jpmidi-0.2.tar.gz
+	cp ./src/jpmidi /usr/local/bin
+	cd ../..
+	rm -f jpmidi-0.21.tar.gz
 fi
