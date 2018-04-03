a4e92aca47aa68f356cbe509bf39eb9f14f5d741
Jośe Fernando Moyano <fernando@zauber.es>
2018-04-03 12:43:59 +0200

    No need for this script.

--- sbin/midinet.sh
+++ sbin/midinet.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
 
-export ZYNTHIAN_LOG_LEVEL=10			# 10=DEBUG, 20=INFO, 30=WARNING, 40=ERROR, 50=CRITICAL
-export ZYNTHIAN_RAISE_EXCEPTIONS=0
-
-cd $ZYNTHIAN_UI_DIR
-/usr/local/bin/qmidinet --no-gui --jack-midi=1 --alsa-midi=0 -n=4
