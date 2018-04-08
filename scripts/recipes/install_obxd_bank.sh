6f829388f6414f2b44ea51bfef2faf00e49a7491
jofemodo <fernando@zynthian.org>
2018-04-09 00:24:10 +0200

    Install OBXD presets collection in LV2 format.

---
+++ scripts/recipes/install_obxd_bank.sh
@@ -0,0 +1,7 @@
+#OBXD presets bank
+cd $ZYNTHIAN_PLUGINS_SRC_DIR
+wget http://zynthian.org/download/obxd_bank.tar.bz2
+tar xfvj obxd_bank.tar.bz2
+cd obxd_bank
+cp -a obxd $ZYNTHIAN_MY_DATA_DIR/presets/lv2/obxd.lv2
+cd ..
