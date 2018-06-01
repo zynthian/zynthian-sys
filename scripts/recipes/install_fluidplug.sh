18e87ab4454dc3be7b03dffdddaad6aba0e54f52
jofemodo <fernando@zynthian.org>
2018-06-01 11:22:35 +0200

    Fix problems building fluidplug: Try to use fluidsynth v2 library, but it have to use v1.

--- scripts/recipes/install_fluidplug.sh
+++ scripts/recipes/install_fluidplug.sh
@@ -8,6 +8,11 @@ sed -i -- 's/^DESTDIR =/DESTDIR =\/zynthian\/zynthian-plugins/' Makefile
 sed -i -- 's/^PREFIX  = \/usr//' Makefile
 sed -i -- 's/\$(PREFIX)\/lib//' Makefile
 #cp $ZYNTHIAN_RECIPE_DIR/FluidPlug.Makefile .
+
+#Avoid using FluidSynth v2, installed in /usr/local:
+export C_INCLUDE_PATH="/usr/include:/usr/local/include"
+#mv /usr/local/include/fluidsynth.h /usr/local/include/fluidsynth2.h
+
 #make -f FluidPlug.Makefile
 make -j 4
 #sudo make -f FluidPlug.Makefile install
