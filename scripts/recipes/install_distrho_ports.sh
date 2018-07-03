dde2f795ecb0c8b28852826994385c2e70e6d620
Guy Sheffer <guysoft@gmail.com>
2018-07-03 14:24:29 +0300

    Workaround for #59, does not build ttl on qemu untill we figure out what to do

--- scripts/recipes/install_distrho_ports.sh
+++ scripts/recipes/install_distrho_ports.sh
@@ -9,8 +9,10 @@ git clone https://github.com/DISTRHO/DISTRHO-Ports.git
 cd DISTRHO-Ports
 export LINUX_EMBED=true
 ./scripts/premake-update.sh linux
-#make -j 3 lv2
-make -j 1 lv2
+# Workaround for https://github.com/zynthian/zynthian-sys/issues/59
+# Caused by https://bugs.launchpad.net/qemu/+bug/1776478
+sed -i 's@\t\@./scripts/generate-ttl.sh@\t@g' Makefile
+make -j 3 lv2
 make install
 make clean
 make distclean
