609162fdcf30d368d5da2f137cb849de2f387a6c
Guy Sheffer <guysoft@gmail.com>
2018-08-25 13:21:30 +0300

    Use updated mod-ui, which is now in zynthian-single-commit branch

--- scripts/recipes/install_mod-ui.sh
+++ scripts/recipes/install_mod-ui.sh
@@ -1,6 +1,7 @@
 # mod-ui
 cd $ZYNTHIAN_SW_DIR
 git clone --recursive https://github.com/zynthian/mod-ui.git
+checkout zynthian-single-commit
 cd mod-ui
 
 if [[ -n $MOD_UI_GITSHA ]]; then
@@ -8,7 +9,6 @@ if [[ -n $MOD_UI_GITSHA ]]; then
 	git checkout zynthian
 fi
 
-sed -i "/\b\(pycrypto\)\b/d" requirements.txt
 sudo pip3 install -r requirements.txt
 cd utils
 make
