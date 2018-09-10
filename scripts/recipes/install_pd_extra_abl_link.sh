57b0eb5a99aa740fd15eaf55016a27c8e7fafd53
jofemodo <fernando@zynthian.org>
2018-09-10 18:20:05 +0200

    + Install Jalv (extended for Zynthian integration) + Install Ableton-link support for MOD-HOST and PureData.

---
+++ scripts/recipes/install_pd_extra_abl_link.sh
@@ -0,0 +1,11 @@
+# Install Pure data extra abl_link~ which allows sync with Ableton Link
+
+cd $ZYNTHIAN_SW_DIR
+git clone --recursive https://github.com/libpd/abl_link.git
+cd abl_link/external
+
+make -j 4 CFLAGS="-mfpu=neon-fp-armv8 -mneon-for-64bits -mfloat-abi=hard -mvectorize-with-neon-quad"
+sudo make install
+make clean
+
+cd ../..
