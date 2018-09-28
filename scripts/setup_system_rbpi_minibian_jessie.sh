4ffcda6561c7d1cc847b53d97c45aa0d311d8b4b
jofemodo <fernando@zynthian.org>
2018-09-28 15:12:06 +0200

    + Fix Setup scripts: Install "Ableton Link stuff" after PureData stuff. + LV2 recipe: Try to fix current compilation problems. Create symlink to new lv2.h location.

--- scripts/setup_system_rbpi_minibian_jessie.sh
+++ scripts/setup_system_rbpi_minibian_jessie.sh
@@ -268,11 +268,6 @@ bash $ZYNTHIAN_RECIPE_DIR/install_jackclient-python.sh
 # Install QMidiNet (MIDI over IP Multicast)
 bash $ZYNTHIAN_RECIPE_DIR/install_qmidinet.sh
 
-# Install Ableton Link Support
-bash $ZYNTHIAN_RECIPE_DIR/install_hylia.sh
-bash $ZYNTHIAN_RECIPE_DIR/install_pd_extra_abl_link.sh
-
-
 #************************************************
 #------------------------------------------------
 # Compile / Install Synthesis Software
@@ -320,6 +315,10 @@ cd $ZYNTHIAN_SYS_DIR/scripts
 # Install Plugins
 cd $ZYNTHIAN_SYS_DIR/scripts
 ./setup_plugins_rbpi.sh
+
+# Install Ableton Link Support
+bash $ZYNTHIAN_RECIPE_DIR/install_hylia.sh
+bash $ZYNTHIAN_RECIPE_DIR/install_pd_extra_abl_link.sh
 
 #************************************************
 #------------------------------------------------
