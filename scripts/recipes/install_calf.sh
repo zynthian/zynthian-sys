7f03ead8074d7eaaa1b445ab4b9999dc86ef0a24
jofemodo <fernando@zynthian.org>
2018-10-25 22:30:05 +0200

    Fix #75: Disable calf recipe because build fails. Instead, install from repository an older version.

--- scripts/recipes/install_calf.sh
+++ scripts/recipes/install_calf.sh
@@ -5,6 +5,7 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/calf-studio-gear/calf.git
 cd calf
+git checkout 0.90.1
 ./autogen.sh
 ./configure --with-lv2-dir=$ZYNTHIAN_PLUGINS_DIR/lv2
 make -j 4
