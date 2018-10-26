d35c7e3b4343a8bc95e2703eb4d0f17b0513f964
jofemodo <fernando@zynthian.org>
2018-10-26 18:42:48 +0200

    Fix Pianoteq binary path when creating soft-link in Pianoteq-demo install recipe.

--- scripts/recipes/install_pianoteq_demo.sh
+++ scripts/recipes/install_pianoteq_demo.sh
@@ -6,5 +6,5 @@ tar xfvJ pianoteq6_demo.tar.xz
 mv pianoteq6_demo pianoteq6
 rm pianoteq6_demo.tar.xz
 cd pianoteq6
-ln -s "./arm/Pianoteq 6 STAGE" pianoteq
+ln -s "./Pianoteq 6 STAGE" pianoteq
 ln -s "$ZYNTHIAN_SW_DIR/pianoteq6/Pianoteq 6 STAGE.lv2" "$ZYNTHIAN_PLUGINS_DIR/lv2"
