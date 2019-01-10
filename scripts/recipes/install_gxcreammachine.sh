ccb9dca03f3d9ed35f95eab80555690feac03e96
Steve Baker <steve@stevebaker.org>
2019-01-10 00:07:32 +0000

    Install the brummer10 guitarix mod plugins
    This is a very good collection of plugins, and is where a lot of new moddevices
    pedals come from

--- scripts/recipes/install_gxcreammachine.sh
+++ scripts/recipes/install_gxcreammachine.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# install_gxvintagefuzz.sh
+# install_gxcreammachine.sh
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
-git clone https://github.com/moddevices/GxVintageFuzzMaster.lv2.git
-cd GxVintageFuzzMaster.lv2
-sed -i -- 's/-msse2 -mfpmath=sse//' Makefile
+rm -rf GxCreamMachine.lv2
+git clone https://github.com/brummer10/GxCreamMachine.lv2.git
+cd GxCreamMachine.lv2/
 sed -i -- 's/INSTALL_DIR = \/usr\/lib\/lv2/INSTALL_DIR = \/zynthian\/zynthian-plugins\/lv2/' Makefile
-make -j 4
+make check clean nogui mod
 sudo make install
 make clean
 cd ..
