5ef212e25190403f88c96b315437710bf26f998e
Steve Baker <steve@stevebaker.org>
2019-01-11 03:02:36 +0000

    Fix install path for mod-pitchshifter
    Without this change, all of these plugins end up installed to the same
    directory, and they don't show up in mod-ui

--- scripts/recipes/install_mod-pitchshifter.sh
+++ scripts/recipes/install_mod-pitchshifter.sh
@@ -2,9 +2,10 @@
 
 # install_mod-pitchshifter.sh
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
+rm -rf mod-pitchshifter
 git clone https://github.com/moddevices/mod-pitchshifter.git
 cd mod-pitchshifter
-sed -i -- 's/INSTALLATION_PATH = \$(DESTDIR)\$(INSTALL_PATH)\/\$(EFFECT_PATH)/INSTALLATION_PATH = \/zynthian\/zynthian-plugins\/lv2\/mod-pitchshifter/' Makefile.mk
+sed -i -- 's/INSTALLATION_PATH = \$(DESTDIR)\$(INSTALL_PATH)\/\$(EFFECT_PATH)/INSTALLATION_PATH = \/zynthian\/zynthian-plugins\/lv2\/\$(PLUGIN).lv2/' Makefile.mk
 make -j 4 NOOPT=true
 sudo make install
 make clean
