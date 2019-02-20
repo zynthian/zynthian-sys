7b6646b3ad86bdb3c0767862faccbcbf50cb9ff5
jofemodo <fernando@zynthian.org>
2019-02-20 15:15:54 +0100

    Fix Calf LV2 plugin install recipe. Disable LADSPA plugins install, as is not needed anymore.

--- scripts/recipes/install_calf.sh
+++ scripts/recipes/install_calf.sh
@@ -5,12 +5,12 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/calf-studio-gear/calf.git
 cd calf
-git checkout 0.90.1
 ./autogen.sh
-./configure --with-lv2-dir=$ZYNTHIAN_PLUGINS_DIR/lv2
 make -j 4
 make install
 make clean
+
+ln -s /usr/local/lib/lv2/calf.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
 
 # Prevent mod-lv2-data from disabling most calf plugins by overwriting manifest.ttl
 # with the few calf plugins that have mod-ui interfaces
