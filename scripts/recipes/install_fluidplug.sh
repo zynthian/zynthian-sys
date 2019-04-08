f5f6cd9b292ede5bd3287beb39561e2d6228db90
jofemodo <fernando@zynthian.org>
2019-04-08 19:12:21 +0200

    Fix FluidPlug recipe for working with FluidSynth 2.0.4.

--- scripts/recipes/install_fluidplug.sh
+++ scripts/recipes/install_fluidplug.sh
@@ -2,23 +2,17 @@
 
 # fluidplug
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
-git clone https://github.com/falkTX/FluidPlug.git
+#git clone https://github.com/falkTX/FluidPlug.git
+git clone https://github.com/zynthian/FluidPlug.git
 cd FluidPlug/
 sed -i -- 's/-ffast-math -mtune=generic -msse -msse2 -mfpmath=sse -fdata-sections -ffunction-sections//' Makefile.mk
 sed -i -- 's/-msse -msse2//' Makefile.mk
 sed -i -- 's/^DESTDIR =/DESTDIR =\/zynthian\/zynthian-plugins/' Makefile
 sed -i -- 's/^PREFIX  = \/usr//' Makefile
 sed -i -- 's/\$(PREFIX)\/lib//' Makefile
-#cp $ZYNTHIAN_RECIPE_DIR/FluidPlug.Makefile .
 
-#Avoid using FluidSynth v2, installed in /usr/local:
-export C_INCLUDE_PATH="/usr/include:/usr/local/include"
-#mv /usr/local/include/fluidsynth.h /usr/local/include/fluidsynth2.h
-
-#make -f FluidPlug.Makefile
 make -j 4
-#sudo make -f FluidPlug.Makefile install
-sudo make install
-sudo ldconfig
+make install
+ldconfig
 make distclean
 cd ..
