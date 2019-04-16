97ac098dbff293cb933042087d9567c0e150e6db
jofemodo <fernando@zynthian.org>
2019-04-17 01:50:58 +0200

    Fix jack-smf-utils install/upgrade.

--- scripts/recipes.update.jessie/00_install_packages.sh
+++ scripts/recipes.update.jessie/00_install_packages.sh
@@ -11,7 +11,7 @@ if [ "$res" == "" ]; then
 fi
 
 # 2017-11-22 => jack-smf-utils, playback and record MID files
-if [ ! -d "$ZYNTHIAN_SW_DIR/jack-smf-utils-1.0" ]; then
+if [ ! -d "$ZYNTHIAN_SW_DIR/jack-smf-utils" ]; then
 	$ZYNTHIAN_RECIPE_DIR/install_jack-smf-utils.sh
 fi
 
@@ -190,7 +190,3 @@ if [ "$res" != "Status: install ok installed" ]; then
 	apt-get -y install xloadimage imagemagick
 fi
 
-# 2019-04-10: Install jack-smf-utils from zynthian repository
-if [ ! -d "$ZYNTHIAN_SW_DIR/jack-smf-utils" ]; then
-	$ZYNTHIAN_RECIPE_DIR/install_jack-smf-utils.sh
-fi
