06a53a797dbc484f0fb7301a908a25a00c296ac4
jofemodo <fernando@zynthian.org>
2019-05-07 17:05:58 +0200

    Fix jack2 install recipe for using version 1.9.12. This recipe is not currently used by setup.

--- scripts/recipes/pending/install_jack2.sh
+++ scripts/recipes/pending/install_jack2.sh
@@ -1,10 +1,10 @@
 # Jack2
 cd $ZYNTHIAN_SW_DIR
-sudo apt-get install -y libasound2-dev
 git clone https://github.com/jackaudio/jack2.git
 cd jack2
+git checkout 1.9.12
 ./waf configure
 ./waf build
-sudo ./waf install 
+./waf install 
 ./waf clean
 cd ..
