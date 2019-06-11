bfc13f25d6b10bfac13abd40d297a13b6c2b876f
jofemodo <fernando@zynthian.org>
2019-06-11 13:17:27 +0200

    Add "/usr/local/lib" to "/etc/ld.so.conf.d" (system wide) instead of using LD_LIBRARY_PATH on every script.

--- sbin/zynthian.sh
+++ sbin/zynthian.sh
@@ -6,7 +6,6 @@ else
 	source "$ZYNTHIAN_SYS_DIR/scripts/zynthian_envars.sh"
 fi
 
-export LD_LIBRARY_PATH=/usr/local/lib
 export PYTHONFAULTHANDLER=1
 
 export ZYNTHIAN_LOG_LEVEL=10			# 10=DEBUG, 20=INFO, 30=WARNING, 40=ERROR, 50=CRITICAL
