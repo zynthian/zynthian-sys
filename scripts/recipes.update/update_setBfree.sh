ef43d87f670d23a0b7aabd3cd92481eee3d1571a
jofemodo <fernando@zynthian.org>
2019-07-10 15:24:23 +0200

    Fix regex for correctly matching git output when repos are up-to-date.

--- scripts/recipes.update/update_setBfree.sh
+++ scripts/recipes.update/update_setBfree.sh
@@ -1,7 +1,7 @@
 
 cd $ZYNTHIAN_SW_DIR/setBfree
 git checkout .
-git pull | grep -q -v 'Already up-to-date.' && changed=1
+git pull | grep -q -v 'Already up.to.date.' && changed=1
 if [[ "$changed" -eq 1 ]]; then
 	sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" common.mak
 	sed -i -- 's/^lv2dir = \$(PREFIX)\/lib\/lv2/lv2dir = \/zynthian\/zynthian-plugins\/lv2/' common.mak
