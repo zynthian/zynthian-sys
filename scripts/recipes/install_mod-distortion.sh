f3a5983b074bd8892c89e6d31f7a549a6f8e8f44
jofemodo <fernando@zynthian.org>
2019-07-18 02:29:39 +0200

    Try to solve #112: mod-distortion runs forever and fail the build. Disabled make concurrency...

--- scripts/recipes/install_mod-distortion.sh
+++ scripts/recipes/install_mod-distortion.sh
@@ -4,7 +4,7 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/moddevices/mod-distortion.git
 cd mod-distortion
-make -j 4
-sudo make INSTALL_PATH=$ZYNTHIAN_PLUGINS_DIR/lv2 install
+make
+make INSTALL_PATH=$ZYNTHIAN_PLUGINS_DIR/lv2 install
 make clean
 cd ..
