ad6e7e5548c839aeb04a6eb7094902c1e4f7d891
jofemodo <fernando@zynthian.org>
2019-08-14 03:05:58 +0200

    Fix #113: caps-lv2 plugins are not working on Aruk. Recompiling solves the problem.

--- scripts/recipes.update.stretch/00_install_packages.sh
+++ scripts/recipes.update.stretch/00_install_packages.sh
@@ -1,2 +1,10 @@
 
-# Nothing to update ...
+
+# 2019-08-14: Build mod-caps LV2 plugins that wan't correctly built on Aruk RC-3
+date_caps_lv2=`stat -c %y $ZYNTHIAN_PLUGINS_SRC_DIR/caps-lv2`
+if [ "$date_caps_lv2" \< "2019-08-01" ]; then
+	$ZYNTHIAN_RECIPE_DIR/install_mod-caps.sh
+fi
+
+
+
