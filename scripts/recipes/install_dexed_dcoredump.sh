23c1da1139b63a9e4a1fc8049391e3619e4a2fa9
lguyome45 <53947617+lguyome45@users.noreply.github.com>
2019-08-19 09:45:51 +0200

    Add files via upload

--- scripts/recipes/install_dexed_dcoredump.sh
+++ scripts/recipes/install_dexed_dcoredump.sh
@@ -2,6 +2,21 @@
 
 
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
+
+#Add the source download of lvtk 
+git clone https://github.com/lvtk/lvtk.git
+cd lvtk
+
+#Dexed need lvtk/synth.hpp is not in the master branch but in branch 1.x
+git checkout 1.x
+./waf configure
+./waf build
+sudo ./waf install
+
+#Dexed Makefile need lvtk-plugin-2.pc, need to rename
+sudo mv /usr/local/lib/pkgconfig/lvtk-plugin-1.pc /usr/local/lib/pkgconfig/lvtk-plugin-2.pc
+cd ..
+
 git clone https://github.com/dcoredump/dexed
 cd dexed
 git checkout native-lv2
@@ -9,3 +24,5 @@ cd src
 make
 sudo make install
 cd ../..
+
+
