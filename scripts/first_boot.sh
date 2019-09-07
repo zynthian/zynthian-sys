38de88997221ef33da662081a507885329191692
lguyome45 <53947617+lguyome45@users.noreply.github.com>
2019-09-07 17:35:12 +0200

    Update first_boot.sh
    Add the script to generate the presets of Helm on the first run

--- scripts/first_boot.sh
+++ scripts/first_boot.sh
@@ -6,6 +6,10 @@ source "/zynthian/config/zynthian_envars.sh"
 # Regenerate Keys
 $ZYNTHIAN_SYS_DIR/sbin/regenerate_keys.sh
 
+# Mke the presets for Helm engine
+$ZYNTHIAN_SYS_DIR/scripts/recipes/make_helm_presets.sh
+
+
 # Enable WIFI AutoAccessPoint (hostapd)
 systemctl unmask hostapd
 
