e1cd18a4f743de0b656ade23fa96381f522fb6b8
jofemodo <fernando@zynthian.org>
2019-10-07 10:59:16 +0200

    Update Pianoteq Trial version to v660.

--- scripts/recipes/install_pianoteq_demo.sh
+++ scripts/recipes/install_pianoteq_demo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export PIANOTEQ_INSTALL_FILENAME="pianoteq_stage_linux_trial_v660.7z"
+
 cd $ZYNTHIAN_SW_DIR
-wget http://zynthian.org/download/pianoteq_stage_linux_trial_v651.7z
-$ZYNTHIAN_RECIPE_DIR/install_pianoteq_binary.sh "$ZYNTHIAN_SW_DIR/pianoteq_stage_linux_trial_v651.7z"
-rm -f "$ZYNTHIAN_SW_DIR/pianoteq_stage_linux_trial_v651.7z"
+wget http://zynthian.org/download/$PIANOTEQ_INSTALL_FILENAME
+$ZYNTHIAN_RECIPE_DIR/install_pianoteq_binary.sh "$ZYNTHIAN_SW_DIR/$PIANOTEQ_INSTALL_FILENAME"
+rm -f "$ZYNTHIAN_SW_DIR/$PIANOTEQ_INSTALL_FILENAME"
 
 $ZYNTHIAN_SYS_DIR/scripts/update_zynthian_sys.sh
