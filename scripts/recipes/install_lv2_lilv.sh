e1617523f744ab45cc771839813b2fae17c3ab05
jofemodo <fernando@zynthian.org>
2019-10-30 17:43:30 +0100

    Remove deprecated waf option that was breaking lilv build.

--- scripts/recipes/install_lv2_lilv.sh
+++ scripts/recipes/install_lv2_lilv.sh
@@ -49,26 +49,13 @@ cd ..
 git clone --recursive https://github.com/drobilla/lilv.git
 #git clone --recursive http://git.drobilla.net/lilv.git lilv
 cd lilv
-#./waf configure --bindings --python=/usr/bin/python2
+#./waf configure --python=/usr/bin/python2
 #./waf build
 #./waf install
 #./waf clean
-./waf configure --bindings --python=/usr/bin/python3
+./waf configure --python=/usr/bin/python3
 ./waf build
 ./waf install
-if [ -d "/usr/lib/python3.4" ]; then
-	cp ./bindings/python/lilv.py /usr/lib/python3.4
-elif [ -d "/usr/lib/python3.5" ]; then
-	cp ./bindings/python/lilv.py /usr/lib/python3.5
-elif [ -d "/usr/lib/python3.7" ]; then
-	cp ./bindings/python/lilv.py /usr/lib/python3.7
-fi
 ./waf clean
 cd ..
-
-git clone https://github.com/brunogola/lilv_python_examples
-cd lilv_python_examples
-2to3 -w *.py
-export PYTHONPATH="/usr/local/lib/python3/dist-packages"
-python3 lv2ls.py
 
