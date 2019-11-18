6aae904c32558b3ad661894462c0a8ab8841f326
jofemodo <fernando@zynthian.org>
2019-11-18 01:07:22 +0100

    Fix recipes for installing on zynthian update.

--- scripts/recipes.update.stretch/00_install_packages.sh
+++ scripts/recipes.update.stretch/00_install_packages.sh
@@ -9,7 +9,7 @@ else
 fi
 
 # 2019-11-18: Install preset2lv2
-if [ -d $ZYNTHIAN_SW_DIR/preset2lv2 ]; then
+if [ ! -d $ZYNTHIAN_SW_DIR/preset2lv2 ]; then
 	$ZYNTHIAN_RECIPE_DIR/install_preset2lv2.sh
 else
 	echo "... preset2lv2 already installed!"
