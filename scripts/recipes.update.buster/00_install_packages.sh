169d7688492d0a863df081e47da4d26c8ffdc05d
jofemodo <fernando@zynthian.org>
2019-11-18 01:03:53 +0100

    Add preset2lv2 to the party ;-)

--- scripts/recipes.update.buster/00_install_packages.sh
+++ scripts/recipes.update.buster/00_install_packages.sh
@@ -1,2 +1,7 @@
 
-# Nothing to update ...
+# 2019-11-18: Install preset2lv2
+if [ -d $ZYNTHIAN_SW_DIR/preset2lv2 ]; then
+	$ZYNTHIAN_RECIPE_DIR/install_preset2lv2.sh
+else
+	echo "... preset2lv2 already installed!"
+fi
