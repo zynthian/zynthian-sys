f5010dacf77c5f5e27e97fcbbbca93fb76add615
jofemodo <fernando@zynthian.org>
2019-11-26 15:13:37 +0100

    Change triceratops source repoitory for using the original.

--- scripts/recipes/install_triceratops.sh
+++ scripts/recipes/install_triceratops.sh
@@ -2,8 +2,14 @@
 
 # triceratops.lv2
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
-git clone https://github.com/BlokasLabs/triceratops.lv2.git
-cd triceratops.lv2
+
+
+if [ -d "triceratops" ]; then
+	rm -rf "triceratops"
+fi
+#git clone https://github.com/BlokasLabs/triceratops.lv2.git
+git clone https://github.com/thunderox/triceratops.git
+cd triceratops
 sed -i -- "s/'-O2'/'-O2','-fPIC'/" wscript
 ./waf configure
 ./waf
@@ -11,4 +17,12 @@ sed -i -- "s/'-O2'/'-O2','-fPIC'/" wscript
 ./waf clean
 cd ..
 
+if [ -d "$ZYNTHIAN_PLUGINS_DIR/lv2/triceratops.lv2" ]; then
+	rm -rf "$ZYNTHIAN_PLUGINS_DIR/lv2/triceratops.lv2"
+fi
 ln -s "/usr/local/lib/lv2/triceratops.lv2" "$ZYNTHIAN_PLUGINS_DIR/lv2"
+
+if [ -d "$ZYNTHIAN_PLUGINS_DIR/lv2/triceratops-presets.lv2" ]; then
+	rm -rf "$ZYNTHIAN_PLUGINS_DIR/lv2/triceratops-presets.lv2"
+fi
+ln -s "/usr/local/lib/lv2/triceratops-presets.lv2" "$ZYNTHIAN_PLUGINS_DIR/lv2"
