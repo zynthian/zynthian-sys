672d48dbe0479d70e659ce98b34c13d711a0390c
jofemodo <fernando@zynthian.org>
2019-11-28 21:08:47 +0100

    New LV2 plugin (synth engine): amsynth!

--- scripts/recipes.update.buster/00_install_packages.sh
+++ scripts/recipes.update.buster/00_install_packages.sh
@@ -2,6 +2,9 @@
 # 2019-11-18: Install preset2lv2
 if [ ! -d $ZYNTHIAN_SW_DIR/preset2lv2 ]; then
 	$ZYNTHIAN_RECIPE_DIR/install_preset2lv2.sh
-else
-	echo "... preset2lv2 already installed!"
+fi
+
+# 2019-11-28: Install amsynth
+if [ ! -d $ZYNTHIAN_PLUGINS_DIR/lv2/amsynth.lv2 ]; then
+	$ZYNTHIAN_RECIPE_DIR/install_amsynth.sh
 fi
