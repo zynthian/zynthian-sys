3b7020822b5440d2e453777478298f92b40a0891
jofemodo <fernando@zynthian.org>
2019-12-02 19:57:28 +0100

    Add SOUNDCARD_MIXER configuration.

--- scripts/zynthian_envars.sh
+++ scripts/zynthian_envars.sh
@@ -26,6 +26,7 @@ export ZYNTHIAN_KIT_VERSION="V3-PRO"
 #Audio Config
 export SOUNDCARD_NAME="HifiBerry DAC+ ADC PRO"
 export SOUNDCARD_CONFIG="dtoverlay=hifiberry-dacplusadcpro,slave"
+export SOUNDCARD_MIXER="Digital,ADC"
 export JACKD_OPTIONS="-P 70 -t 2000 -s -d alsa -d hw:sndrpihifiberry -r 44100 -p 256 -n 2 -X raw"
 
 #Display Config
