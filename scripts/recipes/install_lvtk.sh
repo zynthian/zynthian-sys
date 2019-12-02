8d56236139447c1277f29c65d56d2e4cf55fc8ff
jofemodo <fernando@zynthian.org>
2019-12-02 15:26:33 +0100

    Install both versions (1 & 2)of LVTK.

--- scripts/recipes/install_lvtk.sh
+++ scripts/recipes/install_lvtk.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-# LVtk
-cd $ZYNTHIAN_SW_DIR
 if [ -d lvtk ]; then
 	rm -rf lvtk
 fi
-git clone https://github.com/lvtk/lvtk.git
-cd lvtk
-#git checkout 1.x
-./waf configure --disable-ui
+
+# LVTK-1
+cd $ZYNTHIAN_SW_DIR
+if [ -d lvtk-1 ]; then
+	rm -rf lvtk-1
+fi
+git clone https://github.com/lvtk/lvtk.git lvtk-1
+cd lvtk-1
+git checkout 1.x
+#./waf configure --disable-ui
+./waf configure
 ./waf build
 ./waf install
 ./waf clean
 cd ..
-apt-get install -y lvtk-tools
+
+# LVTK-2
+cd $ZYNTHIAN_SW_DIR
+if [ -d lvtk-2 ]; then
+	rm -rf lvtk-2
+fi
+git clone https://github.com/lvtk/lvtk.git lvtk-2
+cd lvtk-2
+#./waf configure --disable-ui
+./waf configure
+./waf build
+./waf install
+./waf clean
+cd ..
+
 cp /usr/local/lib/pkgconfig/lvtk-plugin-1.pc /usr/local/lib/pkgconfig/lvtk-plugin-2.pc
