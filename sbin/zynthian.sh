93a34617bff8150249dab17c75ae6a67810e1e3b
jofemodo <fernando@zynthian.org>
2019-12-13 21:49:00 +0100

    Fix zynthian services to correctly stop the UI + Fix display back-light control.

--- sbin/zynthian.sh
+++ sbin/zynthian.sh
@@ -10,4 +10,5 @@ export PYTHONFAULTHANDLER=1
 export ZYNTHIAN_LOG_LEVEL=10			# 10=DEBUG, 20=INFO, 30=WARNING, 40=ERROR, 50=CRITICAL
 
 cd $ZYNTHIAN_UI_DIR
-startx ./zynthian.sh -- :0 vt3
+startx ./zynthian.sh
+#startx ./zynthian.sh -- :0 vt3
