5157544da503783422ad9335b8a20c4ece7732ef
jofemodo <fernando@zynthian.org>
2019-12-17 13:50:30 +0100

    Fix setBfree-controller recipe for installing in the correct LV2 dir for zynthian.

--- scripts/recipes/install_setBfree-controller.sh
+++ scripts/recipes/install_setBfree-controller.sh
@@ -4,7 +4,7 @@
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 git clone https://github.com/vallsv/setbfree-controller.lv2
 cd setbfree-controller.lv2
-sed -i -- "s|\$(DESTDIR)\$(PREFIX)/lib/lv2|$ZYNTHIAN_PLUGINS_DIR/lv2|" Makefile
+sed -i -- "s|\$(DESTDIR)\$(PREFIX)\$(SYSPATH)|\$(ZYNTHIAN_PLUGINS_DIR)/lv2|" Makefile
 make -j 3
 make install
 cd ..
