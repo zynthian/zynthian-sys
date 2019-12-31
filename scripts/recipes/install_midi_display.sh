494a0aba8c1b82af171a17b62ab78cf3798cbec8
jofemodo <fernando@zynthian.org>
2019-12-31 02:25:32 +0100

    Improve latest plugin recipes and add them to plugins setup.

--- scripts/recipes/install_midi_display.sh
+++ scripts/recipes/install_midi_display.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
+if [ -d "midi-display.lv2" ]; then
+	rm -rf "midi-display.lv2"
+fi
 git clone https://github.com/vallsv/midi-display.lv2.git
 cd midi-display.lv2
 make -j 3
-DESTDIR=/zynthian/zynthian-plugins PREFIX=/lv2 SYSPATH="" make install
+DESTDIR=$ZYNTHIAN_PLUGINS_DIR PREFIX=/lv2 SYSPATH="" make install
+make clean
 cd ..
--- scripts/recipes/install_string-machine.sh
+++ scripts/recipes/install_string-machine.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
+if [ -d "string-machine" ]; then
+	rm -rf "string-machine"
+fi
 git clone --recursive https://github.com/jpcima/string-machine.git
 cd string-machine
 make -j 3
-DESTDIR=/zynthian/zynthian-plugins LV2DIR=/lv2 make install
+DESTDIR=$ZYNTHIAN_PLUGINS_DIR LV2DIR=/lv2 make install
+make clean
 cd ..
