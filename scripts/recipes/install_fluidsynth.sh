b6d098810f04c97b5bd5efb875158828260f0ff4
jofemodo <fernando@zynthian.org>
2020-01-22 15:12:42 +0100

    Temporary fix #133: Use older v2.0.9 to avoid problems that could be related with recently introduced bugs (v2.1.0).

--- scripts/recipes/install_fluidsynth.sh
+++ scripts/recipes/install_fluidsynth.sh
@@ -2,8 +2,16 @@
 
 # fluidsynth
 cd $ZYNTHIAN_SW_DIR
-git clone https://github.com/FluidSynth/fluidsynth.git
-cd fluidsynth
+
+SW_DIR="fluidsynth"
+if [ -d "$SW_DIR" ]; then
+	rm -rf "$SW_DIR"
+fi
+
+#git clone https://github.com/FluidSynth/fluidsynth.git
+#git clone --branch v2.1.0 https://github.com/FluidSynth/fluidsynth.git $SW_DIR
+git clone --branch v2.0.9 https://github.com/FluidSynth/fluidsynth.git $SW_DIR
+cd $SW_DIR
 mkdir build
 cd build
 cmake .. -Denable-alsa=0 -Denable-aufile=0 -Denable-oss=0 -Denable-pulseaudio=0 -Denable-ladspa=0 -Denable-lash=0 -Denable-floats=1 
@@ -11,4 +19,6 @@ cmake .. -Denable-alsa=0 -Denable-aufile=0 -Denable-oss=0 -Denable-pulseaudio=0 
 make -j 4
 make install
 ldconfig
+
 cd ../..
+rm -rf "$SW_DIR"
