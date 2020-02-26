ada0085c3977d0d7985139e5ee8db7a14a8c01e6
jofemodo <fernando@zynthian.org>
2020-02-26 12:04:23 +0100

    Fix update_jalv script for correctly changing to master branch.

--- scripts/recipes.update/update_jalv.sh
+++ scripts/recipes.update/update_jalv.sh
@@ -1,14 +1,18 @@
 
 cd $ZYNTHIAN_SW_DIR/jalv
 
+# Change to Master branch if needed
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 is_zynthian_branch="$(echo $branch_name | grep zynthian)"
 if [[ "$is_zynthian_branch" -ne "" ]]; then
-	# Change to Master branch
-	rm -rf build
-	rm -f .lock*
-	git checkout .
-	git checkout master
+	cd ..
+	rm -rf jalv
+	git clone --recursive https://github.com/zynthian/jalv.git
+	cd jalv
+	./waf configure
+	./waf build
+	./waf install
+	exit
 fi
 
 git pull | grep -q -v 'Already up.to.date.' && changed=1
@@ -17,4 +21,3 @@ if [[ "$changed" -eq 1 ]]; then
 	./waf build
 	./waf install
 fi
-cd ..
