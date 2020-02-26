d5d61cd51b0db08b02bc9ce91c0f12037da1ae27
jofemodo <fernando@zynthian.org>
2020-02-26 12:06:27 +0100

    Fix typo from last commit.

--- scripts/recipes.update/update_jalv.sh
+++ scripts/recipes.update/update_jalv.sh
@@ -4,7 +4,7 @@ cd $ZYNTHIAN_SW_DIR/jalv
 # Change to Master branch if needed
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 is_zynthian_branch="$(echo $branch_name | grep zynthian)"
-if [[ "$is_zynthian_branch" -ne "" ]]; then
+if [[ "$is_zynthian_branch" != "" ]]; then
 	cd ..
 	rm -rf jalv
 	git clone --recursive https://github.com/zynthian/jalv.git
