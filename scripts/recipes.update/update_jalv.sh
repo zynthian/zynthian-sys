23fdaa92f2f3509ee174ed3479a0d8af752c3e72
jofemodo <fernando@zynthian.org>
2020-02-26 11:55:00 +0100

    Modify update_jalv script for start using master branch.

--- scripts/recipes.update/update_jalv.sh
+++ scripts/recipes.update/update_jalv.sh
@@ -1,6 +1,17 @@
 
 cd $ZYNTHIAN_SW_DIR/jalv
-git pull origin zynthian | grep -q -v 'Already up.to.date.' && changed=1
+
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
+is_zynthian_branch="$(echo $branch_name | grep zynthian)"
+if [[ "$is_zynthian_branch" -ne "" ]]; then
+	# Change to Master branch
+	rm -rf build
+	rm -f .lock*
+	git checkout .
+	git checkout master
+fi
+
+git pull | grep -q -v 'Already up.to.date.' && changed=1
 if [[ "$changed" -eq 1 ]]; then
 	./waf configure
 	./waf build
