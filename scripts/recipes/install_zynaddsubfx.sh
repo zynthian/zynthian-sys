5ea96f0ef8995b52ac0cebccda30ed659a946ead
jofemodo <fernando@zynthian.org>
2020-02-28 12:30:53 +0100

    Fix ZynAddSubFX build by using an older version. Latest version from repo doesn't build.

--- scripts/recipes/install_zynaddsubfx.sh
+++ scripts/recipes/install_zynaddsubfx.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Don't use environment optimizations
 #unset CFLAGS
 #unset CXXFLAGS
@@ -11,13 +10,20 @@ if [[ ${MACHINE_HW_NAME} =~ [armv7l] ]]; then
 	export CXXFLAGS=$CFLAGS
 fi
 
+
 cd $ZYNTHIAN_SW_DIR
+if [ -d "zynaddsubfx" ]; then
+	rm -rf "zynaddsubfx"
+fi
+
 git clone https://github.com/fundamental/zynaddsubfx.git
 cd zynaddsubfx
+# Checkout last commit that builds OK
+git checkout 2454b5dd1041b77a33faa740053885f0256a48fb
 
 #Temporal Fix => Should be removed when PR merged!
 #---------------
-touch HISTORY.txt
+#touch HISTORY.txt
 #---------------
 
 mkdir build
@@ -27,6 +33,7 @@ cmake ..
 # => delete "-msse -msse2 -mfpmath=sse" 
 # => optimizations: -pipe -mfloat-abi=hard -mfpu=neon-vfpv4 -mvectorize-with-neon-quad -funsafe-loop-optimizations -funsafe-math-optimizations
 # => optimizations that doesn't work: -mcpu=cortex-a7 -mtune=cortex-a7
+
 sed -i -- "s/-march=armv7-a -mfloat-abi=hard -mfpu=neon -mcpu=cortex-a9 -mtune=cortex-a9 -pipe -mvectorize-with-neon-quad -funsafe-loop-optimizations/$CFLAGS $CFLAGS_UNSAFE/" CMakeCache.txt
 cmake ..
 
@@ -39,3 +46,5 @@ make install
 
 #Create soft links to LV2 plugins
 ln -s /usr/local/lib/lv2/Zyn*.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
+
+rm -rf "$ZYNTHIAN_SW_DIR/zynaddsubfx"
