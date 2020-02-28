a1a16d4456b414d69df2c7b620f73f84e470f432
jofemodo <fernando@zynthian.org>
2020-02-28 12:30:53 +0100

    Fix ZynAddSubFX build by using an older version. Latest version from repo doesn't build.

--- scripts/recipes/install_zynfusion.sh
+++ scripts/recipes/install_zynfusion.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-apt-get install ruby rake
 apt-get install libgl1-mesa-swx11
 
 cd $ZYNTHIAN_SW_DIR
@@ -11,3 +10,4 @@ ruby build-rpi3.rb
 tar -jxvf zyn-fusion-linux-3.0.3-patch1-rpi3-release.tar.bz2
 cd zyn-fusion
 bash ./install-linux.sh
+
