ca1fbf5a84233b090083cd258128e8721a7c6746
jofemodo <fernando@zynthian.org>
2020-02-28 14:21:46 +0100

    Fix build problems on several plugin install recipes.

--- scripts/recipes/install_infamous.sh
+++ scripts/recipes/install_infamous.sh
@@ -2,25 +2,25 @@
 
 # infamous
 
-. $ZYNTHIAN_RECIPE_DIR/_zynth_lib.sh
+export CXXFLAGS="$CFLAGS -fpermissive"
 
 cd $ZYNTHIAN_SW_DIR/plugins
-
-zynth_git https://github.com/ssj71/infamousPlugins.git
-if [ ${?} -ne 0 -o  "${build}" = "build" ]
-then
-	zynth_build_request clear
-	cd infamousPlugins
-	mkdir build
-	cd build
-	cmake -DLIBDIR="" -DCMAKE_INSTALL_PREFIX=${ZYNTHIAN_PLUGINS_DIR} ..
-	for i in `find src -name cmake_install.cmake`
-	do
-		sed -i -- "s/lib\/lv2/lv2/" $i
-	done
-	make -j 4
-	make install
-	mv /zynthian/zynthian-plugins/bin/infamous-rule /usr/local/bin
-	make clean
-	cd ..
+if [ -d "infamousPlugins" ]; then
+	rm -rf "infamousPlugins"
 fi
+
+git clone https://github.com/ssj71/infamousPlugins.git
+cd infamousPlugins
+mkdir build
+cd build
+cmake -DLIBDIR="" -DCMAKE_INSTALL_PREFIX=${ZYNTHIAN_PLUGINS_DIR} ..
+for i in `find src -name cmake_install.cmake`; do
+	sed -i -- "s/lib\/lv2/lv2/" $i
+done
+make -j 4
+make install
+mv /zynthian/zynthian-plugins/bin/infamous-rule /usr/local/bin
+make clean
+cd ..
+
+rm -rf "infamousPlugins"
--- scripts/recipes/install_openav-artyfx.sh
+++ scripts/recipes/install_openav-artyfx.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 # openav-artyfx.sh
+
+export CXXFLAGS="$CFLAGS -fpermissive"
+
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
+if [ -d "openAV-ArtyFX" ]; then
+	rm -rf "openAV-ArtyFX"
+fi
+
 git clone https://github.com/openAVproductions/openAV-ArtyFX.git
 cd openAV-ArtyFX
 sed -i -- 's/ lib\/lv2\// \/zynthian\/zynthian-plugins\/lv2\//' CMakeLists.txt
-cmake -DHAVE_NTK=OFF
+#cmake -DBUILD_GUI=OFF -DBUILD_SSE=OFF .
+cmake .
 make -j 4
 make install
 make clean
 cd ..
+
+rm -rf "openAV-ArtyFX"
