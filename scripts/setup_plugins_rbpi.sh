355fc73ab252d23cb09c76587400af3cc33aaaa7
jofemodo <fernando@zynthian.org>
2020-03-03 11:31:22 +0100

    Add X42-meters to setup script.

--- scripts/setup_plugins_rbpi.sh
+++ scripts/setup_plugins_rbpi.sh
@@ -115,6 +115,7 @@ $ZYNTHIAN_RECIPE_DIR/install_reMID.sh
 #$ZYNTHIAN_RECIPE_DIR/install_ADLplug.sh
 $ZYNTHIAN_RECIPE_DIR/install_miniopl3.sh
 $ZYNTHIAN_RECIPE_DIR/install_ykchorus.sh
+$ZYNTHIAN_RECIPE_DIR/install_meters.sh
 
 $ZYNTHIAN_RECIPE_DIR/install_lv2_plugins_prebuilt.sh
 
