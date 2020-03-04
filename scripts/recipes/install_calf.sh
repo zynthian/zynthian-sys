f3891951307828e6fa7ba94b263ed39e3525b949
jofemodo <fernando@zynthian.org>
2020-03-04 17:54:21 +0100

    Send all the strange linker .strtab-related messages to /dev/null. In fact, i'm sending to /dev/null the full stderr output ;-0

--- scripts/recipes/install_calf.sh
+++ scripts/recipes/install_calf.sh
@@ -3,18 +3,20 @@
 # Calf plugins
 
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
+if [ -d "calf" ]; then
+	rm -rf "calf"
+fi
+
 git clone https://github.com/calf-studio-gear/calf.git
 cd calf
 ./autogen.sh
-make -j 4
+make -j 4 2>/dev/null
 make install
 make clean
 
-ln -s /usr/local/lib/lv2/calf.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
-
 # Prevent mod-lv2-data from disabling most calf plugins by overwriting manifest.ttl
 # with the few calf plugins that have mod-ui interfaces
-rm -rf $ZYNTHIAN_PLUGINS_DIR/lv2/calf_zynth.lv2
-mv $ZYNTHIAN_PLUGINS_DIR/lv2/calf.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2/calf_zynth.lv2
+ln -s /usr/local/lib/lv2/calf.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2/calf_zynth
 
 cd ..
+rm -rf "calf"
