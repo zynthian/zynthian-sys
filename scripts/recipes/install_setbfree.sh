733a50dc6e833584efb8a15cea4613b28fbd35c2
jofemodo <fernando@zynthian.org>
2020-03-11 18:56:12 +0100

    Fix @146: Remove bad LV2 bundles causing error messages.

--- scripts/recipes/install_setbfree.sh
+++ scripts/recipes/install_setbfree.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd $ZYNTHIAN_SW_DIR
-rm -rf setBfree
+if [ -d "setBfree" ]; then
+	rm -rf "setBfree"
+fi
+
 git clone https://github.com/pantherb/setBfree.git
 cd setBfree
 mkdir lv2
@@ -9,19 +12,15 @@ mkdir lv2
 sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" common.mak
 sed -i -- "s|^lv2dir = \$(PREFIX)/lib/lv2|lv2dir = $ZYNTHIAN_SW_DIR/setBfree/lv2|" common.mak
 
-# First, make b_whirl_mod
-MOD=true make -j 4
-MOD=true make install
-mv lv2/b_whirl lv2/b_whirl_mod
-
-# Now make b_whirl
-make clean
 make -j 4
 make install
+cp -a lv2/b_synth $ZYNTHIAN_PLUGINS_DIR/lv2/b_synth.lv2
+cp -a lv2/b_whirl $ZYNTHIAN_PLUGINS_DIR/lv2/b_whirl.lv2
 
-# Split extended into b_whirl_xt
-cp -r lv2/b_whirl lv2/b_whirl_xt
-rm lv2/b_whirl/b_whirl-configurable.ttl
-rm lv2/b_whirl_xt/b_whirl.ttl
+# make b_whirl_mod
+#make clean
+#MOD=true make -j 4
+#cp -a lv2/b_whirl $ZYNTHIAN_PLUGINS_DIR/lv2/b_whirl_mod.lv2
 
-cp -r lv2/* /zynthian/zynthian-plugins/lv2/
+cd ..
+rm -rf "setBfree"
