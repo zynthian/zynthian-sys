a022b5aa47824a5ab60be7c7058713c3d0741048
jofemodo <fernando@zynthian.org>
2020-04-10 12:24:04 +0200

    Install njconnect Jack Graph Manager.

--- scripts/setup_system_rbpi_raspbian_lite_stretch.sh
+++ scripts/setup_system_rbpi_raspbian_lite_stretch.sh
@@ -320,6 +320,9 @@ $ZYNTHIAN_RECIPE_DIR/install_dxsyx.sh
 # Install preset2lv2 (Convert native presets to LV2)
 $ZYNTHIAN_RECIPE_DIR/install_preset2lv2.sh
 
+# Install the njconnect Jack Graph Manager
+$ZYNTHIAN_RECIPE_DIR/install_njconnect.sh
+
 #************************************************
 #------------------------------------------------
 # Compile / Install Synthesis Software
