72ef1c057144ab61f80a76efd22756604e53a21c
Holger Wirtz <dcoredump@googlemail.com>
2020-05-24 11:28:45 +0100

    New script based on work from hexdump0815.

--- scripts/recipes/install_surge.sh
+++ scripts/recipes/install_surge.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+# https://github.com/hexdump0815/surge-arm-build
+# https://github.com/nemequ/simde
 
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 sudo apt-get install --no-install-recommends --yes build-essential libcairo2-dev libxkbcommon-x11-dev libxkbcommon-dev libxcb-cursor-dev libxcb-keysyms1-dev libxcb-util0-dev
-git clone https://github.com/falkTX/surge.git
+git clone https://github.com/hexdump0815/surge-arm-build.git
+git checkout 5add960506adc2af3a647698edb8fca3f6ef07f0 # just to be sure to use the right version!
+cd surge-arm-build
+git clone https://github.com/surge-synthesizer/surge.git
 cd surge
+git checkout release_1.6.6
 git submodule update --init --recursive
+patch --forward -p0 <../surge-armv7l.patch
+find * -type f -exec ../simde-ify.sh {} \;
+git clone https://github.com/nemequ/simde.git
+sed -i 's/flags { "StaticRuntime" }/flags { staticruntime "On" }/g' premake5.lua
 premake5 --cc=gcc --os=linux gmake2
-sed -i 's/config=debug_x64/config=release_armv7ve/g' surge-lv2.make
-sed -i 's/-msse2/-DNEON_SSE=1/g' surge-lv2.make
-CXXFLAGS=`echo $CXXFLAGS | sed 's/-munaligned-access //'` make -f surge-lv2.make 
-rsync -r --delete target/lv2/Release/Surge.lv2 ${ZYNTHIAN_PLUGINS_DIR}/lv2
+sed -i '/^ifndef verbose/,+4d' surge-lv2.make
+sed -i 's, -m64,,g;s,-msse2,-march=armv7-a -mfpu=vfpv3-d16 -mno-unaligned-access,g;s,FORCE_INCLUDE +=,FORCE_INCLUDE += -Isimde/simde,g' surge-*.make
+CXXFLAGS="" CFLAGS="" make -f surge-lv2.make 
+rsync -r --delete target/lv2/Debug/Surge.lv2 "${ZYNTHIAN_PLUGINS_DIR}/lv2"
+cd ..
