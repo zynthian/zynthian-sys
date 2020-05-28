b872be285148af6c1ae00db6a5c93199296d83e3
jofemodo <fernando@zynthian.org>
2020-05-28 18:08:04 +0200

    Improve latest recipes (delete directories before/after installing, etc.)

--- scripts/recipes/install_arpeggiator.sh
+++ scripts/recipes/install_arpeggiator.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
+if [ -d "arpeggiator_LV2" ]; then
+	rm -rf "arpeggiator_LV2"
+fi
+
 git clone https://github.com/BramGiesen/arpeggiator_LV2.git
 cd arpeggiator_LV2
 sed -i 's#^PREFIX  := /usr#PREFIX  := #' Makefile
@@ -9,3 +13,5 @@ sed -i "s#^DESTDIR :=#DESTDIR := $ZYNTHIAN_PLUGINS_DIR#" Makefile
 make -j3
 make install
 cd ..
+
+rm -rf "arpeggiator_LV2"
--- scripts/recipes/install_premake5.sh
+++ scripts/recipes/install_premake5.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-cd $ZYNTHIAN_PLUGINS_SRC_DIR
+cd $ZYNTHIAN_SW_DIR
+if [ -d "premake-core" ]; then
+	rm -rf "premake-core"
+fi
+
 git clone https://github.com/premake/premake-core.git
 cd premake-core
 make -j3 -f Bootstrap.mak linux 
-#premake5 gmake
-premake5 embed
-#bin/release/premake5 test
-cp bin/release/premake5 /usr/local/bin/premake5
+#./bin/release/premake5 gmake
+./bin/release/premake5 embed
+#./bin/release/premake5 test
+cp ./bin/release/premake5 /usr/local/bin/premake5
 cd ..
+
+rm -rf "premake-core"
