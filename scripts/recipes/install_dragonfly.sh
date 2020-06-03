4c01b0f1f8a1d4bb80b32729b0e44f4d065be87e
jofemodo <fernando@zynthian.org>
2020-06-03 04:02:52 +0200

    Fix install script for working with DragonFly v3.0.0

--- scripts/recipes/install_dragonfly.sh
+++ scripts/recipes/install_dragonfly.sh
@@ -10,7 +10,6 @@ if [ -d "dragonfly-reverb" ]; then
 	rm -rf "dragonfly-reverb"
 fi
 
-rm -rf dragonfly-reverb
 git clone --recurse-submodules https://github.com/michaelwillis/dragonfly-reverb.git
 cd dragonfly-reverb
 git checkout $DRAGONFLY_RELEASE
@@ -32,24 +31,9 @@ index 8f5dec1..93d1484 100644
 EOF
 
 cd dpf
-git apply - <<EOF
-diff --git a/Makefile.base.mk b/Makefile.base.mk
-index 90c22da..21628e2 100644
---- a/Makefile.base.mk
-+++ b/Makefile.base.mk
-@@ -90,7 +90,7 @@ endif
- # Set build and link flags
- 
- BASE_FLAGS = -Wall -Wextra -pipe -MD -MP
--BASE_OPTS  = -O3 -ffast-math -mtune=generic -msse -msse2 -fdata-sections -ffunction-sections
-+BASE_OPTS  = -O3 -ffast-math -fdata-sections -ffunction-sections
- 
- ifeq (\$(MACOS),true)
- # MacOS linker flags
-EOF
-
+make -j 4
 cd ..
-make
+make -j 4
 cp -rp bin/*.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
 
 cd ..
