b72c6178716712a23ae99d925623a3c9461e589d
mheidt <markus@heidt-tech.com>
2020-06-06 12:04:11 +0200

    Update config-on-boot.sh
    corrected bash filename

--- sbin/config-on-boot.sh
+++ sbin/config-on-boot.sh
@@ -2,9 +2,9 @@
 
 UPDATE_SYS_FLAG_FILE="/zynthian_update_sys"
 
-# If flag-file does exist, call zynthian_update_sys.sh ...
+# If flag-file does exist, call update_zynthian_sys.sh ...
 if [ -f $UPDATE_SYS_FLAG_FILE ]; then
-	$ZYNTHIAN_SYS_DIR/scripts/zynthian_update_sys.sh
+	$ZYNTHIAN_SYS_DIR/scripts/update_zynthian_sys.sh
 	rm -f "$UPDATE_SYS_FLAG_FILE"
 fi
 
