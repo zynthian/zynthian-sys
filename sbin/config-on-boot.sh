433e0a6d229ab0150ed407209a263244203bf06f
jofemodo <fernando@zynthian.org>
2020-06-07 19:48:26 +0200

    Improve "config_on_boot" script: When "zynthian_envars.sh" does exist on "/boot" directory, replace the zynthian config and run the update config script.

--- sbin/config-on-boot.sh
+++ sbin/config-on-boot.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+BOOT_CONFIG_FILE="/boot/zynthian_envars.sh"
+
+# If "zynthian_envars.sh" exist on "/boot" directory, replace zynthian config file with it ...
+if [ -f $BOOT_CONFIG_FILE ]; then
+	mv -f $BOOT_CONFIG_FILE $ZYNTHIAN_CONFIG_DIR
+	$ZYNTHIAN_SYS_DIR/scripts/zynthian_update_sys.sh
+	exit
+fi
+
 UPDATE_SYS_FLAG_FILE="/zynthian_update_sys"
 
-# If flag-file does exist, call zynthian_update_sys.sh ...
+# If flag-file does exist, call zynthian_update_sys.sh and remove flag ...
 if [ -f $UPDATE_SYS_FLAG_FILE ]; then
 	$ZYNTHIAN_SYS_DIR/scripts/zynthian_update_sys.sh
-	rm -f "$UPDATE_SYS_FLAG_FILE"
+	rm -f $UPDATE_SYS_FLAG_FILE
+	exit
 fi
+
 
