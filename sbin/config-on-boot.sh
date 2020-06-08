2d5d2dd9f9f48e26d1acba6b99ddda7deb6e6e46
Markus Heidt <markus@heidt-tech.com>
2020-06-08 11:52:40 +0100

    zynthian-webconf#81 ensure that zynthian_envar.sh is called

--- sbin/config-on-boot.sh
+++ sbin/config-on-boot.sh
@@ -2,8 +2,14 @@
 
 UPDATE_SYS_FLAG_FILE="/zynthian_update_sys"
 
+if [ -z "$ZYNTHIAN_CONFIG_DIR" ]; then
+	echo "running /zynthian/config/zynthian_envars.sh"
+	source /zynthian/config/zynthian_envars.sh
+fi
+
 # If flag-file does exist, call update_zynthian_sys.sh ...
 if [ -f $UPDATE_SYS_FLAG_FILE ]; then
+	echo "running update_zynthian.sys.sh after boot"
 	$ZYNTHIAN_SYS_DIR/scripts/update_zynthian_sys.sh
 	rm -f "$UPDATE_SYS_FLAG_FILE"
 fi
