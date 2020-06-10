ac50148dc8c0c0498cde85f6f393b748e21b4ecd
mheidt <markus@heidt-tech.com>
2020-06-10 09:52:49 +0200

    Update config-on-boot.sh
    update_zynthian_sys instead of zynthian_update_sys

--- sbin/config-on-boot.sh
+++ sbin/config-on-boot.sh
@@ -14,7 +14,7 @@ UPDATE_SYS_FLAG_FILE="/zynthian_update_sys"
 # If flag-file does exist, call zynthian_update_sys.sh and remove flag ...
 if [ -f $UPDATE_SYS_FLAG_FILE ]; then
 	echo "Found Config-On-Boot flag! Updating Zynthian config ..."
-	$ZYNTHIAN_SYS_DIR/scripts/zynthian_update_sys.sh
+	$ZYNTHIAN_SYS_DIR/scripts/update_zynthian_sys.sh
 	rm -f $UPDATE_SYS_FLAG_FILE
 	exit
 fi
