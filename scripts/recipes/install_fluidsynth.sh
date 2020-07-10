20a4c4ba1ae7868ba72a9e0798b7f0dd6ada721a
jofemodo <fernando@zynthian.org>
2020-07-10 19:05:32 +0200

    Testing new versions of fluidsynth, but still no success.

--- scripts/recipes/install_fluidsynth.sh
+++ scripts/recipes/install_fluidsynth.sh
@@ -9,15 +9,24 @@ if [ -d "$SW_DIR" ]; then
 fi
 
 #git clone https://github.com/FluidSynth/fluidsynth.git
-#git clone --branch v2.1.0 https://github.com/FluidSynth/fluidsynth.git $SW_DIR
+#git clone --branch v2.1.4 https://github.com/FluidSynth/fluidsynth.git $SW_DIR
 git clone --branch v2.0.9 https://github.com/FluidSynth/fluidsynth.git $SW_DIR
 cd $SW_DIR
 mkdir build
 cd build
 cmake .. -Denable-alsa=0 -Denable-aufile=0 -Denable-oss=0 -Denable-pulseaudio=0 -Denable-ladspa=0 -Denable-lash=0 -Denable-floats=1 
-# Debugging ...
-#-Denable-ubsan=1 -Denable-debug=1 -fsanitize=address
+
+# Debug build ...
+# apt install libubsan0 libasan5
+#export CMAKE_CXX_FLAGS_DEBUG="${CMAKE_CXX_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=address"
+#export CMAKE_LINKER_FLAGS_DEBUG="${CMAKE_LINKER_FLAGS_DEBUG} -fno-omit-frame-pointer -fsanitize=address"
+#cmake .. -Denable-alsa=0 -Denable-aufile=0 -Denable-oss=0 -Denable-pulseaudio=0 -Denable-ladspa=0 -Denable-lash=0 -Denable-floats=1 -Denable-ubsan=1 -Denable-debug=1
+# -fsanitize=address
 #-Denable-fpe-check=0  -Denable-dbus=1
+# Debug Run
+# export LD_PRELOAD=/usr/lib/gcc/arm-linux-gnueabihf/8/libasan.so;
+#/usr/local/bin/fluidsynth -p fluidsynth -a jack -m jack -g 1 -j -o synth.midi-bank-select=mma -o synth.cpu-cores=3 -o synth.polyphony=64
+
 make -j 4
 make install
 ldconfig
