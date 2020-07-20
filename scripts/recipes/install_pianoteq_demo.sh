efbd63c56e267f07bf2885cb975df2fe0df0c1c7
jofemodo <fernando@zynthian.org>
2020-07-20 16:47:08 +0200

    Update pianoteq binary download path.

--- scripts/recipes/install_pianoteq_demo.sh
+++ scripts/recipes/install_pianoteq_demo.sh
@@ -3,7 +3,7 @@
 export PIANOTEQ_INSTALL_FILENAME="pianoteq_stage_linux_trial_v660.7z"
 
 cd $ZYNTHIAN_SW_DIR
-wget http://zynthian.org/download/$PIANOTEQ_INSTALL_FILENAME
+wget http://download.zynthian.org/$PIANOTEQ_INSTALL_FILENAME
 $ZYNTHIAN_RECIPE_DIR/install_pianoteq_binary.sh "$ZYNTHIAN_SW_DIR/$PIANOTEQ_INSTALL_FILENAME"
 rm -f "$ZYNTHIAN_SW_DIR/$PIANOTEQ_INSTALL_FILENAME"
 
