b9e11a9179f211f31c3d686f334f4b98a75d2308
S73V3 <smiths.73v3@gmail.com>
2020-08-09 17:37:11 +0100

    Fix issue with sh vs. bash portability in epdf_detect.sh
    It seems that sh is used in some cases rather than bash.
    Proper quoting and portable = comparison now used. Removed
    negative exit values which also caused problems for sh.
    This should insulate against further unexpected results.

--- scripts/epdf_detect.sh
+++ scripts/epdf_detect.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 HAT_PRODUCT_FILE="/proc/device-tree/hat/product"
+#HAT_PRODUCT_FILE="/proc/device-tree/soc/firmware/compatible"
 HAT_PRODUCT_MATCH="EPDF"
 
-if [ -f $HAT_PRODUCT_FILE ]; then
-	HAT_PRODUCT_STR=$(tr -d '\0' <$HAT_PRODUCT_FILE)
-	if [ $HAT_PRODUCT_STR == $HAT_PRODUCT_MATCH ]; then
+if [ -f "$HAT_PRODUCT_FILE" ]; then
+	HAT_PRODUCT_STR="$(tr -d '\0' <$HAT_PRODUCT_FILE)"
+	if [ "$HAT_PRODUCT_STR" = "$HAT_PRODUCT_MATCH" ]; then
 		exit 0
 	else
 		#Not a matching hat
-		exit -2
+		exit 254
 	fi
 fi
 #No hat present
-exit -1
+exit 255
 
