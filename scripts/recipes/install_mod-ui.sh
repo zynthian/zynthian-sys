0d6744181141e4dc4277a18e1f892ec21b2083ea
jofemodo <fernando@zynthian.org>
2020-09-02 03:05:20 +0200

    Change MOD-UI install script for using the updated @steveb's branch (Issue #55).

--- scripts/recipes/install_mod-ui.sh
+++ scripts/recipes/install_mod-ui.sh
@@ -2,9 +2,9 @@
 
 # mod-ui
 cd $ZYNTHIAN_SW_DIR
-git clone --recursive https://github.com/zynthian/mod-ui.git
+#git clone --recursive https://github.com/zynthian/mod-ui.git
+git clone --recursive --single-branch --branch zyn-mod-merge https://github.com/steveb/mod-ui.git
 cd mod-ui
-#git checkout zynthian-single-commit
 
 if [[ -n $MOD_UI_GITSHA ]]; then
 	git branch -f zynthian $MOD_UI_GITSHA
@@ -12,8 +12,11 @@ if [[ -n $MOD_UI_GITSHA ]]; then
 fi
 
 #pip3 install -r requirements.txt
-pip3 install pyserial==2.7 pystache==0.5.4 aggdraw==1.3.7
+#pip3 install pyserial==2.7 pystache==0.5.4 aggdraw==1.3.7
+pip3 install pyserial==3.0 pystache==0.5.4 aggdraw==1.3.11
 pip3 install git+git://github.com/dlitz/pycrypto@master#egg=pycrypto
+#Pillow==6.2.2
+#tornado==4.3
 
 cd utils
 make
