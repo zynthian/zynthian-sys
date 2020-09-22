8d6b1fdc4ef30995eeacd42c4eaee897436a5395
jofemodo <fernando@zynthian.org>
2020-09-22 21:55:51 +0200

    Implements zynthian/zynthian-ui#247: Add filename parameter to Audio & MIDI recording scripts.

--- sbin/jack-smf-recorder.sh
+++ sbin/jack-smf-recorder.sh
@@ -14,17 +14,28 @@ fi
 
 cd $CAPTURE_DIR
 
-# Get filename (next in sequence) to use ...
-if [ -z "$(ls jack_capture_*.mid)" ]; then
-	fname=jack_capture_01
+if [ -z "$1" ]; then
+	jport="ZynMidiRouter:main_out"
 else
-	last_fname=$(ls jack_capture_*.mid | tail -1)
-	last_fname=${last_fname%.mid}
-	fname=`echo $last_fname | awk -F'[^0-9]+' 'sub($NF"$",sprintf("%0*d",length($NF),$NF+1))'`
+	jport=$1
+fi
+
+if [ -z "$2" ]; then
+	# Get filename (next in sequence) to use ...
+	if [ -z "$(ls jack_capture_*.mid)" ]; then
+		fname=jack_capture_01
+	else
+		last_fname=$(ls jack_capture_*.mid | tail -1)
+		last_fname=${last_fname%.mid}
+		fname=`echo $last_fname | awk -F'[^0-9]+' 'sub($NF"$",sprintf("%0*d",length($NF),$NF+1))'`
+	fi
+	fname=$fname.mid
+else
+	fname=$2
 fi
 
 # Capture MIDI using jack-smf-recorder
-/usr/local/bin/jack-smf-recorder -a ZynMidiRouter:main_out $fname.mid
+/usr/local/bin/jack-smf-recorder -a $jport "$fname"
 
 # Write buffers to disk
 sync
