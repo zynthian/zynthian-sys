b74d26c272fbb35cfac5b4890bd4a201fb902a8d
Jośe Fernando Moyano <fernando@zynthian.org>
2020-10-07 19:30:35 +0200

    Add compatibility with older UI branches

--- scripts/update_zynthian_code.sh
+++ scripts/update_zynthian_code.sh
@@ -45,7 +45,16 @@ echo "Updating zynthian-ui ..."
 cd $ZYNTHIAN_UI_DIR
 git checkout .
 git pull | grep -q -v 'Already up.to.date.' && ui_changed=1
-find ./zynlibs -type f -name build.sh -exec {} \;
+if [ -d "zynlibs" ]; then
+	find ./zynlibs -type f -name build.sh -exec {} \;
+else
+	if [ -d "jackpeak" ]; then
+		./jackpeak/build.sh
+	fi
+	if [ -d "zynseq" ]; then
+		./zynseq/build.sh
+	fi
+fi
 
 echo "Updating zynthian-webconf ..."
 cd $ZYNTHIAN_DIR/zynthian-webconf
