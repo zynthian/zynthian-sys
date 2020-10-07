dd0438d9ae00ed433d85c0adc09ea1d9298777ae
jofemodo <fernando@zynthian.org>
2020-10-07 20:28:29 +0200

    Implements #178: Display information about zynthian during ssh connection, by @riban-bw

--- scripts/update_zynthian_sys.sh
+++ scripts/update_zynthian_sys.sh
@@ -279,6 +279,7 @@ if [ -z "$NO_ZYNTHIAN_UPDATE" ]; then
 	cp -a $ZYNTHIAN_SYS_DIR/etc/ld.so.conf.d/* /etc/ld.so.conf.d
 	cp -a $ZYNTHIAN_SYS_DIR/etc/modprobe.d/* /etc/modprobe.d
 	cp -an $ZYNTHIAN_SYS_DIR/etc/vim/* /etc/vim
+	cp -a $ZYNTHIAN_SYS_DIR/etc/update-motd.d/* /etc/update-motd.d
 	# WIFI Hotspot
 	cp -a $ZYNTHIAN_SYS_DIR/etc/hostapd/* /etc/hostapd
 	cp -a $ZYNTHIAN_SYS_DIR/etc/dnsmasq.conf /etc
