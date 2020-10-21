0d4b1abed6f91ed456dcb0ad8c70a54c598d10a2
jofemodo <fernando@zynthian.org>
2020-10-21 15:27:30 +0200

    Try to drop error messages when installing xterm package (2).

--- scripts/recipes/install_terminado.sh
+++ scripts/recipes/install_terminado.sh
@@ -5,9 +5,9 @@ if [ -d "terminado" ]; then
 	rm -rf "terminado"
 fi
 
-pip3 install tornado_xstatic
 # The next command prints an error that i don't know how to avoid, so i send the error to /dev/null for avoid breaking nightly builds
-pip3 install xstatic 2>/dev/null 
+pip3 install tornado_xstatic 2>/dev/null
+pip3 install xstatic 2>/dev/null
 pip3 install XStatic_term.js
 
 git clone https://github.com/jupyter/terminado.git
