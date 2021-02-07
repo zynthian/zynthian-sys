ad4b4be9f3d4778beb54cfacefd7b41ac11efce4
jofemodo <fernando@zynthian.org>
2021-02-07 03:31:17 +0100

    Remove ldconfig call because it's called from library's Makefile + Install from zynthian repo.

--- scripts/recipes/install_MCP4728.sh
+++ scripts/recipes/install_MCP4728.sh
@@ -6,10 +6,9 @@ if [ -d "MCP4728" ]; then
 	rm -rf "MCP4728"
 fi
 
-git clone https://github.com/marko-pi/MCP4728
+git clone https://github.com/zynthian/MCP4728
 cd "MCP4728"
 make
 make install
-ldconfig
 
 #rm -rf "MCP4728"
