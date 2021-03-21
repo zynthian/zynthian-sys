06c1bf707fb3cf7d54d515e3d2a9a1d13987db64
Julien Woillez <jwoillez@gmail.com>
2021-03-21 22:31:20 +0100

    Build a headless version of Jamulus

--- scripts/recipes/install_jamulus.sh
+++ scripts/recipes/install_jamulus.sh
@@ -5,7 +5,7 @@ cd $ZYNTHIAN_SW_DIR
 git clone https://github.com/jamulussoftware/jamulus.git --branch r3_7_0 jamulus
 cd jamulus
 
-qmake  Jamulus.pro
+qmake "CONFIG+=headless" Jamulus.pro
 make all
 make install
 make clean
