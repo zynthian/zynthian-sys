3ddfa785b6934c1a8b30c9b91b8932a61c5f3403
Julien Woillez <jwoillez@gmail.com>
2021-03-22 10:20:47 +0000

    Added Jamulus.ini file perparation to install script

--- scripts/recipes/install_jamulus.sh
+++ scripts/recipes/install_jamulus.sh
@@ -9,3 +9,16 @@ qmake "CONFIG+=headless" Jamulus.pro
 make all
 make install
 make clean
+
+# Prepare Jamulus.ini
+JAMULUSINIFILE="/root/Jamulus.ini"
+NAME64=$(echo "Raspi $(hostname)"|cut -c -16|tr -d $'\n'|base64)
+echo -e "<client>" > ${JAMULUSINIFILE}
+echo -e "  <name_base64>${NAME64}</name_base64>" >> ${JAMULUSINIFILE}
+echo -e "  <autojitbuf>1</autojitbuf>" >> ${JAMULUSINIFILE}
+echo -e "  <jitbuf>3</jitbuf>" >> ${JAMULUSINIFILE}
+echo -e "  <jitbufserver>3</jitbufserver>" >> ${JAMULUSINIFILE}
+echo -e "  <audiochannels>2</audiochannels>" >> ${JAMULUSINIFILE}
+echo -e "  <audioquality>1</audioquality>" >> ${JAMULUSINIFILE}
+echo -e "</client>" >> ${JAMULUSINIFILE}
+
