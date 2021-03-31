3e605e23e4acfc5070ab037dadf40dab5fb0996e
riban <brian@riban.co.uk>
2021-03-31 19:26:55 +0100

    Update the _right_ script!

--- scripts/recipes.update.stretch/00_install_packages.sh
+++ scripts/recipes.update.stretch/00_install_packages.sh
@@ -88,9 +88,3 @@ systemctl enable zynthian-config-on-boot
 if [ ! -e $ZYNTHIAN_PLUGINS_DIR/lv2/bg-arpeggiator.lv2 ]; then
 	$ZYNTHIAN_RECIPE_DIR/install_arpeggiator.sh
 fi
-
-# 2021-03-31: Install riban LV2 plugins
-if [ ! -e $ZYNTHIAN_PLUGINS_DIR/lv2/riban ]; then
-	$ZYNTHIAN_RECIPE_DIR/install_riban_lv2.sh
-fi
-
