5bcb456925932956150f5107ca640f86a2bc6e35
Holger Wirtz <wirtz@zynthian.org>
2021-04-22 10:56:51 +0100

    Several fixes, especially to the manifest.ttl which causes to avoid loading the plgin due to a wrong path information. Also the script now uses ${ZYNTHIAN_DATA_DIR}/soundfonts/sfz/simsam as storage for the instruments and they are soft linked into the installation folder of the bundle.

--- scripts/recipes/install_simsam.sh
+++ scripts/recipes/install_simsam.sh
@@ -9,8 +9,19 @@ make
 mkdir ${ZYNTHIAN_PLUGINS_DIR}/lv2/simsam.lv2
 cp simsam.so ${ZYNTHIAN_PLUGINS_DIR}/lv2/simsam.lv2
 cd ../bundles/simsam.lv2
+sed -i 's/lv2:binary <\.\.\/\.\.\/build/lv2:binary <\/zynthian\/zynthian-plugins\/lv2\/simsam.lv2/' manifest.ttl
 cp -R * ${ZYNTHIAN_PLUGINS_DIR}/lv2/simsam.lv2
-cd ${ZYNTHIAN_PLUGINS_DIR}/lv2/simsam.lv2/sfz
+cd ${ZYNTHIAN_DATA_DIR}/soundfonts/sfz
+mkdir simsam
+cd simsam
 git clone https://gitlab.com/edwillys/salamandergrandpiano.git
 git clone https://gitlab.com/edwillys/francisbaconpiano.git
 git clone https://gitlab.com/edwillys/SteinwaySonsModelB.git
+cd ${ZYNTHIAN_PLUGINS_DIR}/lv2/simsam.lv2/sfz
+rm -rf salamandergrandpiano francisbaconpiano SteinwaySonsModelB
+ln -s ${ZYNTHIAN_DATA_DIR}/soundfonts/sfz/simsam/salamandergrandpiano .
+ln -s ${ZYNTHIAN_DATA_DIR}/soundfonts/sfz/simsam/francisbaconpiano .
+ln -s ${ZYNTHIAN_DATA_DIR}/soundfonts/sfz/simsam/SteinwaySonsModelB .
+cd ${ZYNTHIAN_PLUGINS_SRC_DIR}/simsam.lv2/build
+make clean
+cd ${ZYNTHIAN_PLUGINS_SRC_DIR}
