d4ee706acae8d39250ff8641590490022be731f8
jofemodo <fernando@zynthian.org>
2021-04-22 12:43:35 +0200

    Discard local changes before update jalv.

--- scripts/recipes.update/update_jalv.sh
+++ scripts/recipes.update/update_jalv.sh
@@ -1,20 +1,7 @@
 
 cd $ZYNTHIAN_SW_DIR/jalv
 
-# Change to Master branch if needed
-branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
-is_zynthian_branch="$(echo $branch_name | grep zynthian)"
-if [[ "$is_zynthian_branch" != "" ]]; then
-	cd ..
-	rm -rf jalv
-	git clone --recursive https://github.com/zynthian/jalv.git
-	cd jalv
-	./waf configure
-	./waf build
-	./waf install
-	exit
-fi
-
+git checkout .
 git pull | grep -q -v 'Already up.to.date.' && changed=1
 if [[ "$changed" -eq 1 ]]; then
 	./waf --no-qt4 configure
