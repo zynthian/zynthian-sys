fe0f34bd36c9f34ff10c4243ce1e01c69cdb78c8
Holger Wirtz <wirtz@zynthian.org>
2021-05-03 09:51:15 +0100

    Small fixes.

--- scripts/recipes/install_sfizz.sh
+++ scripts/recipes/install_sfizz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ${ZYNTHIAN_PLUGINS_SRC_DIR}
-sudo apt-get install libxcb-util0-dev libxcb-keysyms1-dev libxcb-xkb-dev libxkbcommon-x11-dev
+sudo apt-get install libxcb-util0-dev libxcb-keysyms1-dev libxcb-xkb-dev libxkbcommon-x11-dev libxkbcommon-x11-dev libxkbcommon0
 git clone https://github.com/sfztools/sfizz.git sfizz.lv2
 cd sfizz.lv2
 mkdir build
@@ -9,6 +9,5 @@ git submodule update --init --recursive
 cmake ..
 make -j4
 make install
-mv /usr/local/lib/lv2/sfizz.lv2 ${ZYNTHIAN_PLUGINS_DIR}/lv2/
 make clean
 cd ${ZYNTHIAN_PLUGINS_SRC_DIR}
