36e814afd82411d1b864dd613f8b2d885eebb0e2
jofemodo <fernando@zynthian.org>
2021-06-20 17:19:15 +0200

    Install riban-lv2 from deb repo instead of using build recipe.

--- scripts/setup_plugins_rbpi.sh
+++ scripts/setup_plugins_rbpi.sh
@@ -48,7 +48,7 @@ apt-get -y install helm hybridreverb2 infamous-plugins invada-studio-plugins-lv2
 apt-get -y install klangfalter-lv2 lsp-plugins lufsmeter-lv2 luftikus-lv2 lv2vocoder
 apt-get -y install mod-cv-plugins mod-distortion mod-pitchshifter mod-utilities moony.lv2
 apt-get -y install noise-repellent obxd-lv2 oxefmsynth pitcheddelay-lv2 pizmidi-plugins
-apt-get -y install regrader rubberband-lv2 safe-plugins shiro-plugins sorcer
+apt-get -y install regrader riban-lv2 rubberband-lv2 safe-plugins shiro-plugins sorcer
 apt-get -y install temper-lv2 tal-plugins-lv2 tap-lv2 teragonaudio-plugins-lv2 vitalium-lv2
 apt-get -y install wolf-shaper wolf-spectrum wolpertinger-lv2
 apt-get -y install x42-plugins zam-plugins zlfo
