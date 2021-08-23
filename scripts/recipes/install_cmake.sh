27ee0f4773b69d3c5f58d187b9e377da0e5e6314
jofemodo <fernando@zynthian.org>
2021-08-23 12:16:17 +0200

    Improve xfce4 config, so it can be easily updated.

--- scripts/recipes/install_cmake.sh
+++ scripts/recipes/install_cmake.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-cd ${ZYNTHIAN_PLUGINS_SRC_DIR}/..
-sudo apt-get install --yes libssl-dev
-git clone https://gitlab.kitware.com/cmake/cmake.git
-cd cmake
+
+$VERSION="v3.20.0-rc4"
+
+# riban LV2 plugins
+cd $ZYNTHIAN_sw_DIR
+
+if [ -d cmake-$VERSION ]; then
+	rm -rf cmake-$VERSION
+fi
+
+apt-get -y install openssl-dev
+
+wget https://gitlab.kitware.com/cmake/cmake/-/archive/v3.20.0-rc4/cmake-$VERSION.tar.gz
+tar xfvz cmake-$VERSION.tar.gz
+cd cmake-$VERSION
 ./bootstrap --prefix=/usr/local
-make -j4
-make install
-make clean
-cd ${ZYNTHIAN_PLUGINS_SRC_DIR}
+./configure
+
