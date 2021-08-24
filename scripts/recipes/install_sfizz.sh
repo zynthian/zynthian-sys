dac32fe64914ee70643c6e6c81be3c77e1ca603e
jofemodo <fernando@zynthian.org>
2021-08-24 16:43:06 +0200

    When running the sfizz install recipe, always try to install the needed packages.

--- scripts/recipes/install_sfizz.sh
+++ scripts/recipes/install_sfizz.sh
@@ -2,12 +2,12 @@
 
 cd ${ZYNTHIAN_SW_DIR}
 
+apt-get -y update --allow-releaseinfo-change
+apt-get -y install libxcb-util0-dev libxcb-keysyms1-dev libxcb-xkb-dev libxkbcommon-x11-dev zenity
+
 if [ -d "sfizz" ]; then
 	rm -rf "sfizz"
 else
-	apt-get -y update --allow-releaseinfo-change
-	apt-get -y install libxcb-util0-dev libxcb-keysyms1-dev libxcb-xkb-dev libxkbcommon-x11-dev zenity
-fi
 
 #git clone https://github.com/sfztools/sfizz.git sfizz
 git clone https://github.com/zynthian/sfizz.git sfizz
