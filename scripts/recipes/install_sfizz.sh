1cbe0e22350c955ade7df9f3c5199507c1081f52
jofemodo <fernando@zynthian.org>
2021-08-26 13:10:16 +0200

    Fix "if" block that was wrongly closed.

--- scripts/recipes/install_sfizz.sh
+++ scripts/recipes/install_sfizz.sh
@@ -7,7 +7,7 @@ apt-get -y install libxcb-util0-dev libxcb-keysyms1-dev libxcb-xkb-dev libxkbcom
 
 if [ -d "sfizz" ]; then
 	rm -rf "sfizz"
-else
+fi
 
 #git clone https://github.com/sfztools/sfizz.git sfizz
 git clone https://github.com/zynthian/sfizz.git sfizz
