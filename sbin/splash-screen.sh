97819340ca825c81da325fda55a44131fed0e5d5
jofemodo <fernando@zynthian.org>
2022-09-19 21:08:34 +0200

    + Simplify splash screen generation, removing raw image generation also for SPI displays (/dev/fb1). + Disabled early raw-splash display. We will use plymouth ;-)

--- sbin/splash-screen.sh
+++ sbin/splash-screen.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Display Zynthian Boot Splash Screen
+exit
+
+# Display Zynthian Boot Splash Screen => Not used anymore!
 if [[ "$FRAMEBUFFER" == "/dev/fb1" ]]; then
 	cat $ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_boot.raw > $FRAMEBUFFER
 fi  
