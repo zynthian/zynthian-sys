6267b082586449efd1c097311260955333311402
jofemodo <fernando@zynthian.org>
2022-09-22 14:41:15 +0200

    Add "build zyncoder" to autoconfig and config-on-boot.

--- sbin/config-on-boot.sh
+++ sbin/config-on-boot.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 BOOT_CONFIG_FILE="/boot/zynthian_envars.sh"
+UPDATE_SYS_FLAG_FILE="/zynthian_update_sys"
+
+function zynthian_postconfig() {
+	source $ZYNTHIAN_CONFIG_DIR/zynthian_envars.sh
+	$ZYNTHIAN_DIR/zyncoder/build.sh
+	rm -rf $ZYNTHIAN_CONFIG_DIR/img
+	rm -f $UPDATE_SYS_FLAG_FILE
+}
 
 # If "zynthian_envars.sh" exist on "/boot" directory, update zynthian config with it ...
 if [ -f $BOOT_CONFIG_FILE ]; then
 	echo "Found On-Boot config file! Updating Zynthian config ..."
 	$ZYNTHIAN_SYS_DIR/sbin/update_envars.py $BOOT_CONFIG_FILE
-	$ZYNTHIAN_DIR/zyncoder/build.sh
+	zynthian_postconfig
 	exit
 fi
 
-UPDATE_SYS_FLAG_FILE="/zynthian_update_sys"
 
 # If flag-file does exist, call update_zynthian_sys.sh and remove flag ...
 if [ -f $UPDATE_SYS_FLAG_FILE ]; then
 	echo "Found Config-On-Boot flag! Updating Zynthian config ..."
 	$ZYNTHIAN_SYS_DIR/scripts/update_zynthian_sys.sh
-	rm -f $UPDATE_SYS_FLAG_FILE
+	zynthian_postconfig
 	exit
 fi
--- sbin/zynthian_autoconfig.py
+++ sbin/zynthian_autoconfig.py
@@ -68,12 +68,14 @@ else:
 if config_name:
 	if config_name != os.environ.get('ZYNTHIAN_KIT_VERSION'):
 		print("Configuring Zynthian for {} ...".format(config_name))
-		
+
+		zyn_dir = os.environ.get('ZYNTHIAN_DIR',"/zynthian")
 		zsys_dir = os.environ.get('ZYNTHIAN_SYS_DIR',"/zynthian/zynthian-sys")
 		zconfig_dir = os.environ.get('ZYNTHIAN_CONFIG_DIR',"/zynthian/config")
 		
 		check_output("cp -a '{}/config/zynthian_envars_{}.sh' '{}/zynthian_envars.sh'".format(zsys_dir, config_name, zconfig_dir), shell=True)
 		check_output("{}/scripts/update_zynthian_sys.sh".format(zsys_dir), shell=True)
+		check_output("source {}/zynthian_envars.sh; {}/zyncoder/build.sh".format(zconfig_dir, zyn_dir), shell=True)
 		check_output("rm -rf {}/img".format(zconfig_dir), shell=True)
 		check_output("reboot", shell=True)
 	else:
