ddd2883f468aa33f2e1304ff4b9ed3032542e12f
jofemodo <fernando@zynthian.org>
2022-11-25 14:54:41 +0100

    Update libsndfile binaries for supporting mp3 & vorbis (ogg) formats.

--- scripts/recipes/install_jack2.sh
+++ scripts/recipes/install_jack2.sh
@@ -7,7 +7,7 @@ if [ -d "$SW_DIR" ]; then
 	rm -rf "$SW_DIR"
 fi
 
-git clone --branch v1.9.14 https://github.com/jackaudio/jack2.git $SW_DIR
+git clone --branch v1.9.21 https://github.com/jackaudio/jack2.git $SW_DIR
 cd jack2
 ./waf configure
 ./waf build
