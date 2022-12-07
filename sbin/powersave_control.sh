6877b5032a07531d23f0583861bf2332c3bc1db8
jofemodo <fernando@zynthian.org>
2022-12-08 00:19:30 +0100

    Improve powersave_control script: Add screensaver disabling.

--- sbin/powersave_control.sh
+++ sbin/powersave_control.sh
@@ -21,12 +21,15 @@ fi
 case $1 in
 	on)
 		xset dpms force off
+		xset s off
 		backlight_off
 		cpufreq-set -g powersave
 		;;
 	off)
-		cpufreq-set -g performance;
+		cpufreq-set -g performance
 		xset dpms force on
+		xset s reset
+		xset s off
 		backlight_on
 		;;
   *)
