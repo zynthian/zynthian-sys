efd167e1bc8a2d9b2c22655e59c3260b97b32c75
jofemodo <fernando@zynthian.org>
2023-07-11 14:44:04 +0200

    Improve first boot logging.

--- scripts/first_boot.sh
+++ scripts/first_boot.sh
@@ -10,29 +10,39 @@ source "$ZYNTHIAN_SYS_DIR/scripts/delayed_action_flags.sh"
 #------------------------------------------------------------------------------
 
 # Hardware Autoconfig
+echo ""
 echo "Running autoconfig..." >> /root/first_boot.log
 $ZYNTHIAN_SYS_DIR/sbin/zynthian_autoconfig.py 2>&1 >> /root/first_boot.log
-run_reboot_flag_action_raw
+if [ -f $REBOOT_FLAGFILE ]; then
+	clean_all_flags
+	echo "Rebooting..." >> /root/first_boot.log
+	reboot
+	exit
+fi
+#run_reboot_flag_action_raw
 
 # Fix ALSA mixer settings
+echo ""
 echo "Fixing ALSA mixer settings..." >> /root/first_boot.log
-$ZYNTHIAN_SYS_DIR/sbin/fix_alsamixer_settings.sh
+$ZYNTHIAN_SYS_DIR/sbin/fix_alsamixer_settings.sh 2>&1 >> /root/first_boot.log
 
 # Regenerate Keys
+echo ""
 echo "Regenerating keys..." >> /root/first_boot.log
-$ZYNTHIAN_SYS_DIR/sbin/regenerate_keys.sh
+$ZYNTHIAN_SYS_DIR/sbin/regenerate_keys.sh 2>&1 >> /root/first_boot.log
 
 # Enable WIFI AutoAccessPoint (hostapd)
+echo ""
 echo "Unmasking WIFI access point service..." >> /root/first_boot.log
 systemctl unmask hostapd
 
 # Regenerate cache LV2
-echo "Regenerating LV2 cache..." >> /root/first_boot.log
 cd $ZYNTHIAN_CONFIG_DIR/jalv
 if [[ "$(ls -1q | wc -l)" -lt 20 ]]; then
-	echo "Regenerating cache LV2..."
+	echo ""
+	echo "Regenerating LV2 cache..." >> /root/first_boot.log
 	cd $ZYNTHIAN_UI_DIR/zyngine
-	python3 ./zynthian_lv2.py
+	python3 ./zynthian_lv2.py  2>&1 >> /root/first_boot.log
 fi
 
 # Disable first_boot service
