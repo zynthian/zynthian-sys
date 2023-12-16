17edbf37abfd514cd98b455c3dbbeef34712e2c7
jofemodo <fernando@zynthian.org>
2023-12-16 17:39:25 +0100

    Update build instructions for Jalv updates. Now it uses "meson" instead of "waf".

--- scripts/recipes.update/update_jalv.sh
+++ scripts/recipes.update/update_jalv.sh
@@ -4,7 +4,11 @@ cd $ZYNTHIAN_SW_DIR/jalv
 git checkout .
 git pull | grep -q -v 'Already up.to.date.' && changed=1
 if [[ "$changed" -eq 1 ]]; then
-	./waf configure
-	./waf build
-	./waf install
+	if [ ! -d  "./build" ]; then
+		meson setup build
+	fi
+	cd build
+	meson compile
+	meson install
+	ldconfig
 fi
