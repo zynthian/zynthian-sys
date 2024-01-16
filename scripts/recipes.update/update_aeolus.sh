504e99888c7da0d5f1aba6835c31608c8bd7dc6d
jofemodo <fernando@zynthian.org>
2024-01-16 10:15:23 +0100

    Fix aeolus config file & update script.

--- scripts/recipes.update/update_aeolus.sh
+++ scripts/recipes.update/update_aeolus.sh
@@ -3,17 +3,18 @@ cd $ZYNTHIAN_SW_DIR
 if [ ! -d "aeolus" ]
 then
 	git clone https://github.com/riban-bw/aeolus.git
-	cd aeolus/source
+	cd aeolus
 	git checkout zynthian
 	changed=1
 else
-	cd aeolus/source
+	cd aeolus
+	git checkout .
 	git pull | grep -q -v 'Already up to date.' && changed=1
 fi
 if [ "$changed" == "1" ]
 then
+	cd source
 	make clean
 	make -j 3
 	make install
 fi
-cd -
