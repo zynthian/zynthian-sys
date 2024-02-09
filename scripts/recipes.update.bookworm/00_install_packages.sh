6c607b4749da5f17507b20252ae50e0e20a5ec8c
jofemodo <fernando@zynthian.org>
2024-02-09 12:07:43 +0100

    Fix update scripts so they don't break things when destiny directoy doesn't exist.

---
+++ scripts/recipes.update.bookworm/00_install_packages.sh
@@ -0,0 +1,56 @@
+
+aptpkgs=""
+
+
+# 2024-01-08: Install alsa-midi (chain_manager)
+#if is_python_module_installed.py alsa-midi; then
+#	pip3 install alsa-midi
+#fi
+
+# 2024-01-08: Install python3-usb (chain_manager)
+#res=`dpkg -s python3-usb 2>&1 | grep "Status:"`
+#if [ "$res" != "Status: install ok installed" ]; then
+#	aptpkgs="$aptpkgs python3-usb"
+#fi
+
+# -----------------------------------------------------------------------------
+# Install/update recipes shouldn't be added below this line!
+# -----------------------------------------------------------------------------
+
+# Hold some packages
+apt-mark unhold raspberrypi-kernel
+apt-mark unhold raspberrypi-sys-mods
+
+# Install needed apt packages
+if [ ! -z "$aptpkgs" ]; then
+	apt-get -y update --allow-releaseinfo-change
+	apt-get -y install $aptpkgs
+fi
+
+# Upgrade System
+if [[ ! "$ZYNTHIAN_SYS_BRANCH" =~ ^stable.* ]] || [[ "$ZYNTHIAN_FORCE_UPGRADE" == "yes" ]]; then
+	if [ -z "$aptpkgs" ]; then
+		apt-get -y update --allow-releaseinfo-change
+	fi
+	#dpkg --configure -a # => Recover from broken upgrade
+	apt-get -y upgrade
+fi
+
+apt-get -y autoremove
+apt-get -y autoclean
+
+# Reinstall firmware to latest stable version
+#apt-get install --reinstall raspberrypi-bootloader raspberrypi-kernel
+
+# Update firmware to a recent version that works OK!!
+#if [[ "$VIRTUALIZATION" == "none" ]] && [[ "$LINUX_KERNEL_VERSION" < "5.15.61-v7l+" ]]; then
+	#echo "LINUX KERNEL VERSION: $LINUX_KERNEL_VERSION"
+	#SKIP_WARNING=1 rpi-update
+	#set_reboot_flag
+#fi
+
+# Install a firmware version that works OK!!
+#if [[ "$LINUX_KERNEL_VERSION" != "5.10.49-v7l+" ]]; then
+#	rpi-update -y dc6dc9bc6692d808fcce5ace9d6209d33d5afbac
+#	set_reboot_flag
+#fi
--- scripts/recipes.update/update_preset2lv2.sh
+++ scripts/recipes.update/update_preset2lv2.sh
@@ -1,7 +1,9 @@
 
-cd $ZYNTHIAN_SW_DIR/preset2lv2
-git pull | grep -q -v 'Already up.to.date.' && changed=1
-if [[ "$changed" -eq 1 ]]; then
-	python3 ./setup.py install
+if [ -d "$ZYNTHIAN_SW_DIR/preset2lv2" ]; then
+	cd $ZYNTHIAN_SW_DIR/preset2lv2
+	git pull | grep -q -v 'Already up.to.date.' && changed=1
+	if [[ "$changed" -eq 1 ]]; then
+		python3 ./setup.py install
+	fi
+	cd ..
 fi
-cd ..
