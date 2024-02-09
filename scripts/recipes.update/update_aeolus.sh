78beb92aa92a2822ac2e4eb1752df4051c235d46
jofemodo <fernando@zynthian.org>
2024-02-09 12:07:43 +0100

    Fix update scripts so they don't break things when destiny directoy doesn't exist.

--- scripts/recipes.update/update_aeolus.sh
+++ scripts/recipes.update/update_aeolus.sh
@@ -1,20 +1,23 @@
+
 cd $ZYNTHIAN_SW_DIR
 
-if [ ! -d "aeolus" ]
-then
+build_aeolus=0
+if [ ! -d "aeolus" ]; then
 	git clone https://github.com/riban-bw/aeolus.git
 	cd aeolus
 	git checkout zynthian
-	changed=1
+	build_aeolus=1
 else
 	cd aeolus
 	git checkout .
-	git pull | grep -q -v 'Already up to date.' && changed=1
+	git pull | grep -q -v 'Already up to date.' && build_aeolus=1
 fi
-if [ "$changed" == "1" ]
-then
+if [ "$build_aeolus" == "1" ]; then
 	cd source
 	make clean
 	make -j 3
 	make install
+	cd ..
 fi
+cd ..
+
--- scripts/recipes.update/update_setBfree.sh
+++ scripts/recipes.update/update_setBfree.sh
@@ -1,13 +1,15 @@
 
-cd $ZYNTHIAN_SW_DIR/setBfree
-git checkout .
-git pull | grep -q -v 'Already up to date.' && changed=1
-if [ "$changed" == "1" ]
-then
-	sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" common.mak
-	sed -i -- 's/^lv2dir = \$(PREFIX)\/lib\/lv2/lv2dir = \/zynthian\/zynthian-plugins\/lv2/' common.mak
-	make clean
-	make -j 3
-	make install
+if [ -d "$ZYNTHIAN_SW_DIR/setBfree" ]; then
+	cd $ZYNTHIAN_SW_DIR/setBfree
+	git checkout .
+	git pull | grep -q -v 'Already up to date.' && changed=1
+	if [ "$changed" == "1" ]
+	then
+		sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" common.mak
+		sed -i -- 's/^lv2dir = \$(PREFIX)\/lib\/lv2/lv2dir = \/zynthian\/zynthian-plugins\/lv2/' common.mak
+		make clean
+		make -j 3
+		make install
+	fi
+	cd ..
 fi
-cd -
