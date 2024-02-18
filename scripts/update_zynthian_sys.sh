a0d862646efc2eca4018273b6d41988fb2071709
riban <brian@riban.co.uk>
2024-02-18 13:56:15 +0000

    Fixes -nocursor option (had wrong (unicode) character for hyphen

--- scripts/update_zynthian_sys.sh
+++ scripts/update_zynthian_sys.sh
@@ -429,7 +429,7 @@ fi
 if [ "$ZYNTHIAN_UI_ENABLE_CURSOR" == "1" ]; then
   X11_SERVER_OPTIONS=""
 else
-	X11_SERVER_OPTIONS="−nocursor"
+	X11_SERVER_OPTIONS="-nocursor"
 fi
 
 # Copy fonts to system directory
