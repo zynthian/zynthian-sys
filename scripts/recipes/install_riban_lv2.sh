ea7e1185b4799996351da049fc902883d3e126d3
jofemodo <fernando@zynthian.org>
2024-04-09 20:37:22 +0200

    + Install some missing engines. + Update engine_config DB.

--- scripts/recipes/install_riban_lv2.sh
+++ scripts/recipes/install_riban_lv2.sh
@@ -3,21 +3,15 @@
 # riban LV2 plugins
 cd $ZYNTHIAN_PLUGINS_SRC_DIR
 
-if [ -d riban ]; then
-	rm -rf riban
+if [ -d riban-lv2-plugins ]; then
+	rm -rf riban-lv2-plugins
 fi
 
-STABLE=12bc8b3ee06312f5feb78cc894bf78107d3e9710
-git clone --recursive https://github.com/riban-bw/lv2.git riban
-git checkout -fq "$STABLE"
 
-cd riban
-for dir in */
-do
-	cd $dir
-	sed -i "s#^PREFIX.*=.*#PREFIX := $ZYNTHIAN_PLUGINS_DIR#" Makefile
-	sed -i "s#^DESTDIR.*=.*#DESTDIR := /lv2#" Makefile
-	make
-	make install
-	cd -
-done
+git clone --recursive https://github.com/riban-bw/lv2-plugins.git riban-lv2-plugins
+cd riban-lv2-plugins
+#STABLE=12bc8b3ee06312f5feb78cc894bf78107d3e9710
+#git checkout -fq "$STABLE"
+make -j 3
+mv bin/lv2/* /usr/local/lib/lv2
+make clean
