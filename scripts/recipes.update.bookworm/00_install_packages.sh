4c7219328c99cb8b3da5fab50b9f0d82d6a36ad1
jofemodo <fernando@zynthian.org>
2024-05-04 13:09:42 +0200

    Remove offending bluetoothd service files from /etc.

--- scripts/recipes.update.bookworm/00_install_packages.sh
+++ scripts/recipes.update.bookworm/00_install_packages.sh
@@ -228,6 +228,11 @@ if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	$ZYNTHIAN_RECIPE_DIR/install_faust_lv2.sh
 fi
 
+patchlevel="20240504.1"
+if [[ "$current_patchlevel" < "$patchlevel" ]]; then
+	echo "APPLYING PATCH $patchlevel ..."
+	rm -rf /etc/systemd/system/bluetooth.service.d
+fi
 
 # 2024-01-08: Install alsa-midi (chain_manager)
 #if is_python_module_installed.py alsa-midi; then
