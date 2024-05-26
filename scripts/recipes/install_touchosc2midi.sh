5a1a0279f56e1c019b82c44fdbad49c85e6938f4
jofemodo <fernando@zynthian.org>
2024-05-26 20:54:16 +0200

    + Install latest pyliblo from repository. + Install touchosc2midi with some dependency patching. It seems to work!

--- scripts/recipes/install_touchosc2midi.sh
+++ scripts/recipes/install_touchosc2midi.sh
@@ -7,8 +7,11 @@ if [ -d "touchosc2midi" ]; then
 fi
 
 git clone https://github.com/zynthian/touchosc2midi.git
+# Risky try, but it seems to work
 cd touchosc2midi
-pip install .
+sed -i "s/python-rtmidi==1.0.0/python-rtmidi/" requirements.txt
+sed -i "s/Cython==0.25.2/Cython/" requirements.txt
+pip3 install .
 cd ..
 
 rm -rf "touchosc2midi"
