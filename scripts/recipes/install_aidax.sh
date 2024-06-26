0dde6f6f226eb2887ac10be6ee1cc97780958d29
jofemodo <fernando@zynthian.org>
2024-06-26 05:45:12 +0200

    + Bump zynthianOS version when updating and fix build date on last image. + Remove AIDAX tar.xz file.

--- scripts/recipes/install_aidax.sh
+++ scripts/recipes/install_aidax.sh
@@ -10,4 +10,5 @@ wget https://github.com/AidaDSP/AIDA-X/releases/download/1.1.0/AIDA-X-1.1.0-linu
 tar xf AIDA-X-1.1.0-linux-arm64.tar.xz
 cp -r AIDA-X-1.1.0/AIDA-X.lv2 /usr/local/lib/lv2/
 
+rm -f AIDA-X-1.1.0-linux-arm64.tar.xz
 rm -rf "AIDA-X-1.1.0"
