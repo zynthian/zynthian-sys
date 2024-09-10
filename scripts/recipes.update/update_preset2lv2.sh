acad8c1908df95a5a6f814a9032bb8f1ac5ab059
jofemodo <fernando@zynthian.org>
2024-09-10 11:57:16 +0200

    Simplify update scripts.

--- scripts/recipes.update/update_preset2lv2.sh
+++
@@ -1,9 +0,0 @@
-
-if [ -d "$ZYNTHIAN_SW_DIR/preset2lv2" ]; then
-	cd $ZYNTHIAN_SW_DIR/preset2lv2
-	git pull | grep -q -v 'Already up.to.date.' && changed=1
-	if [[ "$changed" -eq 1 ]]; then
-		python3 ./setup.py install
-	fi
-	cd ..
-fi
