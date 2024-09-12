498ebda9bdd760e9a57db52d2af2792033f8b848
jofemodo <fernando@zynthian.org>
2024-09-12 17:25:26 +0200

    Fix zynthian_stop.sh script to exit when no "zynthian.sh" process.

--- sbin/zynthian_stop.sh
+++ sbin/zynthian_stop.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 zynthian_pid=$(pgrep zynthian.sh)
-/zynthian/venv/bin/send_osc 1370 /CUIA/EXIT_UI
-while [[ $(pgrep zynthian.sh) == "$zynthian_pid" ]]; do
-	sleep 0.1
-done
+if [[ "$zynthian_pid" ]]; then
+	/zynthian/venv/bin/send_osc 1370 /CUIA/EXIT_UI
+	while [[ $(pgrep zynthian.sh) == "$zynthian_pid" ]]; do
+		sleep 0.1
+	done
+fi
 
