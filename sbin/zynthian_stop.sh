91067c288da0192868cb049c56abdf12a799ef73
jofemodo <fernando@zynthian.org>
2024-09-12 18:43:32 +0200

    Fix (again) zynthian_stop.sh script. Better look for "zynthian_main.py" process.

--- sbin/zynthian_stop.sh
+++ sbin/zynthian_stop.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-zynthian_pid=$(pgrep zynthian.sh)
+zynthian_pid=$(pgrep -f zynthian_main.py)
 if [[ "$zynthian_pid" ]]; then
 	/zynthian/venv/bin/send_osc 1370 /CUIA/EXIT_UI
-	while [[ $(pgrep zynthian.sh) == "$zynthian_pid" ]]; do
+	while [[ $(pgrep -f zynthian_main.py) == "$zynthian_pid" ]]; do
 		sleep 0.1
 	done
 fi
-
