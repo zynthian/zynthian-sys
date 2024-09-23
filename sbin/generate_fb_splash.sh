1e45187f2ce104791996f36ef14510b4d703cac8
jofemodo <fernando@zynthian.org>
2024-09-23 13:42:12 +0200

    Change splash image format to jpg.

--- sbin/generate_fb_splash.sh
+++ sbin/generate_fb_splash.sh
@@ -27,30 +27,5 @@ fi
 echo "Generating splash images for display resolution $DISPLAY_WIDTH x $DISPLAY_HEIGHT..."
 
 convert_options="-resize ${DISPLAY_WIDTH}x -gravity Center -extent ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT} -strip" 
-/usr/bin/convert "$ZYNTHIAN_UI_DIR/img/zynthian_logo_boot.png" $convert_options "$ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_boot.png"
-/usr/bin/convert "$ZYNTHIAN_UI_DIR/img/zynthian_logo_error.png" $convert_options "$ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_error.png"
-
-exit
-
-# This is not used anymore...
-if [[ "$FRAMEBUFFER" == "/dev/fb1" ]]; then
-	echo "Generating Splash Screens for FrameBuffer..."
-
-	/bin/echo 1 > /sys/class/backlight/*/bl_power
-
-	#Generate "Zynthian Error" Splash Screen
-	/usr/bin/fbi -noverbose -T 2 -a --fitwidth -d $FRAMEBUFFER $ZYNTHIAN_UI_DIR/img/zynthian_logo_error.png
-	sleep 1
-	cat $FRAMEBUFFER > $ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_error.raw
-	/usr/bin/fbgrab -d $FRAMEBUFFER $ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_error.png
-
-	#Generate "Zynthian Boot" Splash Screen
-	/usr/bin/fbi -noverbose -T 2 -a --fitwidth -d $FRAMEBUFFER $ZYNTHIAN_UI_DIR/img/zynthian_logo_boot.png
-	sleep 1
-	cat $FRAMEBUFFER > $ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_boot.raw
-	/usr/bin/fbgrab -d $FRAMEBUFFER $ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_boot.png
-
-	killall -9 fbi
-
-	/bin/echo 0 > /sys/class/backlight/*/bl_power
-fi
+/usr/bin/convert "$ZYNTHIAN_UI_DIR/img/zynthian_logo_boot.jpg" $convert_options "$ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_boot.jpg"
+/usr/bin/convert "$ZYNTHIAN_UI_DIR/img/zynthian_logo_error.jpg" $convert_options "$ZYNTHIAN_CONFIG_DIR/img/fb_zynthian_error.jpg"
