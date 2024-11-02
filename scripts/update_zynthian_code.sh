5f0c7c3427ca34d5402d0bbc52eb84116ace2a76
Jofemodo <txino@motxine.matxinet.org>
2024-11-02 21:39:38 +0100

    Fix last commit.

--- scripts/update_zynthian_code.sh
+++ scripts/update_zynthian_code.sh
@@ -42,7 +42,7 @@ if [ "$RESET_ZYNTHIAN_REPOSITORIES" == "1" ]; then
 	git fetch
 	git reset --hard origin/$branch
 	ui_changed=1
-elif [[ $ZYNTHIAN_SYS_BRANCH == $ZYNTHIAN_STABLE_BRANCH* ]]; then
+elif [[ $ZYNTHIAN_ZYNCODER_BRANCH == $ZYNTHIAN_STABLE_BRANCH* ]]; then
   echo -e "\t...repository frozen in stable tag release!"
 else
 	git pull | grep -q -v 'Already up.to.date.' && ui_changed=1
@@ -59,6 +59,8 @@ if [ "$RESET_ZYNTHIAN_REPOSITORIES" == "1" ]; then
 	git fetch
 	git reset --hard origin/$branch
 	ui_changed=1
+elif [[ $ZYNTHIAN_UI_BRANCH == $ZYNTHIAN_STABLE_BRANCH* ]]; then
+  echo -e "\t...repository frozen in stable tag release!"
 else
 	git pull | grep -q -v 'Already up.to.date.' && ui_changed=1
 fi
@@ -74,6 +76,8 @@ if [ "$RESET_ZYNTHIAN_REPOSITORIES" == "1" ]; then
 	git fetch
 	git reset --hard origin/$branch
 	webconf_changed=1
+elif [[ $ZYNTHIAN_WEBCONF_BRANCH == $ZYNTHIAN_STABLE_BRANCH* ]]; then
+  echo -e "\t...repository frozen in stable tag release!"
 else
 	git pull | grep -q -v 'Already up.to.date.' && webconf_changed=1
 fi
