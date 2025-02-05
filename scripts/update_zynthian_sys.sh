0c9e209e90c201d8159d412bd20ae31d5fe63245
jofemodo <fernando@zynthian.org>
2025-02-05 09:33:18 +0100

    Fix VNC systemd service files.

--- scripts/update_zynthian_sys.sh
+++ scripts/update_zynthian_sys.sh
@@ -528,8 +528,10 @@ sed -i -e "s/#ZYNTHIAN_SYS_DIR#/$ZYNTHIAN_SYS_DIR_ESC/g" /etc/systemd/system/vnc
 # noVNC service
 sed -i -e "s/#ZYNTHIAN_SYS_DIR#/$ZYNTHIAN_SYS_DIR_ESC/g" /etc/systemd/system/novnc0.service
 sed -i -e "s/#ZYNTHIAN_SW_DIR#/$ZYNTHIAN_SW_DIR_ESC/g" /etc/systemd/system/novnc0.service
+sed -i -e "s/#ZYNTHIAN_WEBCONF_DIR#/$ZYNTHIAN_WEBCONF_DIR_ESC/g" /etc/systemd/system/novnc0.service
 sed -i -e "s/#ZYNTHIAN_SYS_DIR#/$ZYNTHIAN_SYS_DIR_ESC/g" /etc/systemd/system/novnc1.service
 sed -i -e "s/#ZYNTHIAN_SW_DIR#/$ZYNTHIAN_SW_DIR_ESC/g" /etc/systemd/system/novnc1.service
+sed -i -e "s/#ZYNTHIAN_WEBCONF_DIR#/$ZYNTHIAN_WEBCONF_DIR_ESC/g" /etc/systemd/system/novnc1.service
 # Zynthian Service
 sed -i -e "s/#FRAMEBUFFER#/$FRAMEBUFFER_ESC/g" /etc/systemd/system/zynthian.service
 sed -i -e "s/#ZYNTHIAN_DIR#/$ZYNTHIAN_DIR_ESC/g" /etc/systemd/system/zynthian.service
