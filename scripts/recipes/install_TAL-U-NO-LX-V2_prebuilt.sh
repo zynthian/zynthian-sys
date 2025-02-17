afff77d203119972312573e9ff473211d5ba962f
jofemodo <fernando@zynthian.org>
2025-02-17 11:29:34 +0100

    Fix filename from last commit.

--- scripts/recipes/install_TAL-U-NO-LX-V2_prebuilt.sh
+++ scripts/recipes/install_TAL-U-NO-LX-V2_prebuilt.sh
@@ -4,6 +4,6 @@ BASE_URL_DOWNLOAD="https://os.zynthian.org/plugins/aarch64"
 
 cd $ZYNTHIAN_PLUGINS_DIR/lv2
 
-wget "$BASE_URL_DOWNLOAD/TAL-U-NO-LX_V2.lv2.tar.xz"
-tar xfv "TAL-U-NO-LX_V2.lv2.tar.xz"
-rm -f "TAL-U-NO-LX_V2.lv2.tar.xz"
+wget "$BASE_URL_DOWNLOAD/TAL-U-NO-LX-V2.lv2.tar.xz"
+tar xfv "TAL-U-NO-LX-V2.lv2.tar.xz"
+rm -f "TAL-U-NO-LX-V2.lv2.tar.xz"
