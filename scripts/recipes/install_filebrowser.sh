f625730c9c0517db3d4b93adbaaff6fe673dd403
jofemodo <fernando@zynthian.org>
2025-03-10 11:28:36 +0100

    Fix filebrowser's install & config from last commit.

--- scripts/recipes/install_filebrowser.sh
+++ scripts/recipes/install_filebrowser.sh
@@ -6,7 +6,9 @@ if [ -d filebrowser ]; then
 	rm -rf filebrowser
 fi
 mkdir filebrowser
+cd filebrowser
 wget https://github.com/filebrowser/filebrowser/releases/download/v2.32.0/linux-arm64-filebrowser.tar.gz
 tar xfvz linux-arm64-filebrowser.tar.gz
 rm -f linux-arm64-filebrowser.tar.gz
 cp -a $ZYNTHIAN_SYS_DIR/etc/filebrowser/* .
+cd ..
