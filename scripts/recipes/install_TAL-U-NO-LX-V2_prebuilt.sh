6bbdc752aaafd3868fef5d1d4c54b9f032fa8a48
jofemodo <fernando@zynthian.org>
2025-06-04 11:57:09 +0200

    Upgrade TAL-U-NO-LX-V2 plugin to version 5.0.

--- scripts/recipes/install_TAL-U-NO-LX-V2_prebuilt.sh
+++ scripts/recipes/install_TAL-U-NO-LX-V2_prebuilt.sh
@@ -4,6 +4,9 @@ BASE_URL_DOWNLOAD="https://os.zynthian.org/plugins/aarch64"
 
 cd $ZYNTHIAN_PLUGINS_DIR/lv2
 
+if [ -d "TAL-U-NO-LX-V2.lv2" ]; then
+	rm -rf "TAL-U-NO-LX-V2.lv2"
+fi
 wget "$BASE_URL_DOWNLOAD/TAL-U-NO-LX-V2.lv2.tar.xz"
 tar xfv "TAL-U-NO-LX-V2.lv2.tar.xz"
 rm -f "TAL-U-NO-LX-V2.lv2.tar.xz"
--- scripts/update_zynthian_recipes.sh
+++ scripts/update_zynthian_recipes.sh
@@ -202,11 +202,11 @@ if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	$ZYNTHIAN_RECIPE_DIR/install_ratatouille_prebuilt.sh
 fi
 
-patchlevel="20250214.1"
-if [[ "$current_patchlevel" < "$patchlevel" ]]; then
-	echo "Applying patch $patchlevel ..."
-	$ZYNTHIAN_RECIPE_DIR/install_TAL-U-NO-LX-V2_prebuilt.sh
-fi
+#patchlevel="20250214.1"
+#if [[ "$current_patchlevel" < "$patchlevel" ]]; then
+#	echo "Applying patch $patchlevel ..."
+#	$ZYNTHIAN_RECIPE_DIR/install_TAL-U-NO-LX-V2_prebuilt.sh
+#fi
 
 patchlevel="20250218.1"
 if [[ "$current_patchlevel" < "$patchlevel" ]]; then
@@ -260,6 +260,12 @@ patchlevel="20250527.1"
 if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	echo "Applying patch $patchlevel ..."
 	$ZYNTHIAN_RECIPE_DIR/install_novachord_prebuilt.sh
+fi
+
+patchlevel="20250604.1"
+if [[ "$current_patchlevel" < "$patchlevel" ]]; then
+	echo "Applying patch $patchlevel ..."
+	$ZYNTHIAN_RECIPE_DIR/install_TAL-U-NO-LX-V2_prebuilt.sh
 fi
 
 # -----------------------------------------------------------------------------
