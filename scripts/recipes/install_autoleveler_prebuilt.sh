1e188d93a201ca2439ddc9151b840054afd11faf
jofemodo <fernando@zynthian.org>
2025-08-28 16:06:52 +0200

    Fix filename in AutoLeveler installer.

--- scripts/recipes/install_autoleveler_prebuilt.sh
+++ scripts/recipes/install_autoleveler_prebuilt.sh
@@ -4,9 +4,9 @@ plugins_path="$ZYNTHIAN_PLUGINS_DIR/lv2"
 BASE_URL_DOWNLOAD="https://os.zynthian.org/plugins/aarch64"
 
 cd $plugins_path
-wget "$BASE_URL_DOWNLOAD/AutoLeveler.lv2 v012.zip"
-if [ -f "AutoLeveler.lv2 v012.zip" ]; then
+wget "$BASE_URL_DOWNLOAD/AutoLeveler.lv2.v012.zip"
+if [ -f "AutoLeveler.lv2.v012.zip" ]; then
 	rm -rf ./AutoLeveler.lv2
-	unzip "AutoLeveler.lv2 v012.zip"
-	rm -f "AutoLeveler.lv2 v012.zip"
+	unzip "AutoLeveler.lv2.v012.zip"
+	rm -f "AutoLeveler.lv2.v012.zip"
 fi
