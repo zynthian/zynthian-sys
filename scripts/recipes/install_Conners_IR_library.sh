a02db38bfa373dbb868bfcdaf651ae1d6abcbda0
jofemodo <fernando@zynthian.org>
2025-10-04 16:51:23 +0200

    Add install scripts for IR files & presets.

--- scripts/recipes/install_Conners_IR_library.sh
+++ scripts/recipes/install_Conners_IR_library.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "Installing Conners Impulse Reponse Library..."
+
+if [ ! -d "$ZYNTHIAN_DATA_DIR/files/IRs" ];
+	mkdir -p "$ZYNTHIAN_DATA_DIR/files/IRs"
+fi
 cd $ZYNTHIAN_DATA_DIR/files/IRs
 rm -rf Conners
 wget https://github.com/itsmusician/IR-Library/releases/download/v2.0.0/Conners-IR-Library_v2.0.0.zip
