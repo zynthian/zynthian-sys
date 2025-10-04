81d5e6e96959c19c5a73432de2b7dd4ccde43998
jofemodo <fernando@zynthian.org>
2025-10-04 14:18:48 +0200

    Rename install script (Conners IR library).

--- scripts/recipes/install_Conners_IR_library.sh
+++ scripts/recipes/install_Conners_IR_library.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Installing Conners Impulse Reponse Library..."
-cd $ZYNTHIAN_DATA_DIR/files
-rm -rf IRs
+cd $ZYNTHIAN_DATA_DIR/files/IRs
+rm -rf Conners
 wget https://github.com/itsmusician/IR-Library/releases/download/v2.0.0/Conners-IR-Library_v2.0.0.zip
 unzip Conners-IR-Library_v2.0.0.zip
 rm -f Conners-IR-Library_v2.0.0.zip
-mv IR-Library-main IRs
+mv IR-Library-main Conners
