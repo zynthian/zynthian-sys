84a02d09fd317cd94b436b92fd8ec04a2299d026
jofemodo <fernando@zynthian.org>
2025-10-23 13:02:18 +0200

    Fix install script for MI plugins.

--- scripts/recipes/install_zynMI_prebuilt.sh
+++ scripts/recipes/install_zynMI_prebuilt.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-BASE_URL_DOWNLOAD="https://github.com/PatttF/zynMI/releases/download/BraidsPlaits/zynMI.zip"
+BASE_URL_DOWNLOAD="https://github.com/PatttF/zynMI/releases/download/BraidsPlaits"
 
-rm -rf $ZYNTHIAN_PLUGINS_DIR/lv2/mi_braids.lv2
-rm -rf $ZYNTHIAN_PLUGINS_DIR/lv2/mi_plaits.lv2
-
-wget "$BASE_URL_DOWNLOAD"
+cd $ZYNTHIAN_PLUGINS_DIR/lv2
+if [ -d "./zynMI" ]; then
+	rm -rf ./zynMI
+fi
+if [ -d "./mi_braids.lv2" ]; then
+	rm -rf ./mi_braids.lv2
+fi
+if [ -d "./mi_plaits.lv2" ]; then
+	rm -rf ./mi_plaits.lv2
+fi
+mkdir zynMI
+cd zynMI
+wget "$BASE_URL_DOWNLOAD/zynMI.zip"
 unzip zynMI.zip
-mv zynMI/braids.lv2 mi_braids.lv2
-mv zynMI/plaits.lv2 mi_plaits.lv2
+cd ..
 mv zynMI/*.lv2 .
+mv braids.lv2 mi_braids.lv2
+mv plaits.lv2 mi_plaits.lv2
 rm -rf zynMI
-rm -fzynMI.zip
