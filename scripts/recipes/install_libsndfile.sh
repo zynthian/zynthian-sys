1edc85b7cf5030f2d1059e0064ee5c8e11a790f0
jofemodo <fernando@zynthian.org>
2025-10-28 18:01:21 +0100

    Fix libsndfile install script to install from @riban's forked repository.

--- scripts/recipes/install_libsndfile.sh
+++ scripts/recipes/install_libsndfile.sh
@@ -2,19 +2,25 @@
 
 cd $ZYNTHIAN_SW_DIR
 
-version=1.1.0
-dname=libsndfile-$version
+#version=1.1.0
+#dname=libsndfile-$version
+dname=libsndfile
 
 if [ -d "$dname" ]; then
 	rm -rf $dname
 fi
 
-wget https://github.com/libsndfile/libsndfile/releases/download/$version/$dname.tar.xz
-tar xf $dname.tar.xz
-rm -f $dname.tar.xz
+#wget https://github.com/libsndfile/libsndfile/releases/download/$version/$dname.tar.xz
+#tar xf $dname.tar.xz
+#rm -f $dname.tar.xz
+
+git clone -b zynthian https://github.com/riban-bw/libsndfile
 
 cd $dname
-./configure
+#./configure
+mkdir build
+cd build
+cmake ..
 make -j 3
 make install
 
