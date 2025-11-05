07cb51b9b23f70c889e9ccaa81fb116836ff3f59
jofemodo <fernando@zynthian.org>
2025-11-05 11:11:55 +0100

    Install MI plugin collection: braids, plaits, aether reverb, etc.

--- scripts/recipes/install_zynMI_prebuilt.sh
+++ scripts/recipes/install_zynMI_prebuilt.sh
@@ -10,7 +10,9 @@ rm -rf ./mi_*.lv2
 mkdir zynMI
 cd zynMI
 wget "$BASE_URL_DOWNLOAD/zynMI.zip"
+#wget "$BASE_URL_DOWNLOAD/mi_mutated-arm64.zip"
 unzip zynMI.zip
+#unzip mi_mutated-arm64.zip
 cd ..
 mv zynMI/*.lv2 .
 rm -rf zynMI
