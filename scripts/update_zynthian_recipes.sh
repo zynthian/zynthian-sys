664c68572b9fa0284fdc0e5edcab34e464aba071
jofemodo <fernando@zynthian.org>
2025-11-05 11:11:55 +0100

    Install MI plugin collection: braids, plaits, aether reverb, etc.

--- scripts/update_zynthian_recipes.sh
+++ scripts/update_zynthian_recipes.sh
@@ -401,6 +401,12 @@ if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	$ZYNTHIAN_RECIPE_DIR/install_lv2_just_jalv.sh
 fi
 
+patchlevel="20251105.1"
+if [[ "$current_patchlevel" < "$patchlevel" ]]; then
+	echo "Applying patch $patchlevel ..."
+	$ZYNTHIAN_RECIPE_DIR/install_zynMI_prebuilt.sh
+fi
+
 # -----------------------------------------------------------------------------
 # End of patches section
 # -----------------------------------------------------------------------------
