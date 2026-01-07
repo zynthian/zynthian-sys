bfbd3cea0e75c0d8f847c238d349ab6713770f83
jofemodo <fernando@zynthian.org>
2026-01-07 18:09:47 +0100

    Update OB-Xf.lv2, including presets (not clasified in banks yet).

--- scripts/recipes/install_OB-Xf.sh
+++ scripts/recipes/install_OB-Xf.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-cd $ZYNTHIAN_PLUGINS_SRC_DIR
+cd "$ZYNTHIAN_PLUGINS_SRC_DIR" || exit
 
 if [ -d "OB-Xf" ]; then
-	rm -rf OB-Xf
+	rm -rf "OB-Xf"
 fi
 
 git clone https://github.com/surge-synthesizer/OB-Xf.git
-cd OB-Xf
+cd OB-Xf || exit
+git submodule update --init --recursive
+sed -i "s/VST3 Standalone//" CMakeLists.txt
 cmake -DOBXF_JUCE_FORMATS=LV2 -B Builds/Release .
-echo -e "\ninclude(Builds/Release/_deps/sstplugininfra-src/cmake/git-version-functions.cmake)" >> CMakeLists.txt
-cmake --build Builds/Release --config Release
-mv ./Builds/Release/OB-Xf_artefacts/LV2/OB-Xf.lv2 $ZYNTHIAN_PLUGINS_DIR/lv2
+cmake --build Builds/Release --config Release --target obxf-staged -j 3
 
+rm -rf "$ZYNTHIAN_PLUGINS_DIR/lv2/OB-Xf.lv2"
+mv ./Builds/Release/OB-Xf_artefacts/LV2/OB-Xf.lv2 "$ZYNTHIAN_PLUGINS_DIR/lv2"
+
+# Remove old wrong assets folder
 assets_dir="/root/Documents/Surge Synth Team/OB-Xf"
-if [ ! -d "$assets_dir" ]; then
+if [ -d "$assets_dir" ]; then
+	rm -rf "$assets_dir"
+fi
+# Create system assets dir
+assets_dir="/usr/local/share/Surge Synth Team/OB-Xf"
+if [ -d "$assets_dir" ]; then
+	rm -rf "$assets_dir/Themes"
+	rm -rf "$assets_dir/Patches"
+else
 	mkdir -p "$assets_dir"
 fi
-mv assets/Surge\ Synth\ Team/OB-Xf/* "$assets_dir"
+mv assets/installer/Surge\ Synth\ Team/OB-Xf/* "$assets_dir"
 
-cd ..
+cd .. || exit
 rm -rf OB-Xf
-
--- scripts/update_zynthian_recipes.sh
+++ scripts/update_zynthian_recipes.sh
@@ -296,12 +296,12 @@ if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	ZYNTHIAN_FORCE_APT_UPGRADE="yes"
 fi
 
-patchlevel="20250810.1"
-if [[ "$current_patchlevel" < "$patchlevel" ]]; then
-	echo "Applying patch $patchlevel ..."
-	aptpkgs="$aptpkgs libwebkit2gtk-4.0-dev"
-	$ZYNTHIAN_RECIPE_DIR/install_OB-Xf_prebuilt.sh
-fi
+#patchlevel="20250810.1"
+#if [[ "$current_patchlevel" < "$patchlevel" ]]; then
+#	echo "Applying patch $patchlevel ..."
+#	aptpkgs="$aptpkgs libwebkit2gtk-4.0-dev"
+#	$ZYNTHIAN_RECIPE_DIR/install_OB-Xf_prebuilt.sh
+#fi
 
 #patchlevel="20250810.2"
 #if [[ "$current_patchlevel" < "$patchlevel" ]]; then
@@ -443,6 +443,14 @@ patchlevel="20251230.3"
 if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	echo "Applying patch $patchlevel ..."
 	$ZYNTHIAN_RECIPE_DIR/install_roboverb_prebuilt.sh
+fi
+
+patchlevel="20260107.1"
+if [[ "$current_patchlevel" < "$patchlevel" ]]; then
+	echo "Applying patch $patchlevel ..."
+	aptpkgs="$aptpkgs libwebkit2gtk-4.0-dev"
+	$ZYNTHIAN_RECIPE_DIR/install_OB-Xf_prebuilt.sh
+	regenerate_lv2_presets.sh urn:org.surge-synth-team.OB-Xf-rework
 fi
 
 # -----------------------------------------------------------------------------
