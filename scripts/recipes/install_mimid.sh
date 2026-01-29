fae4c878008e254171014c3f88f1d6f37f826612
jofemodo <fernando@zynthian.org>
2026-01-29 15:03:15 +0100

    Update QMidiArp and MiMi-d plugins.

--- scripts/recipes/install_mimid.sh
+++ scripts/recipes/install_mimid.sh
@@ -8,6 +8,11 @@ if [ -d "polluxsynth-audio-plugins" ]; then
 	rm -rf "polluxsynth-audio-plugins"
 fi
 
+#wget https://github.com/polluxsynth/audio-plugins/archive/refs/tags/2.1.2z.tar.gz
+#tar xfvz 2.1.2z.tar.gz
+#rm -f 2.1.2z.tar.gz
+#mv audio-plugins-2.1.2z polluxsynth-audio-plugins
+
 git clone --recursive -b zynthian https://github.com/polluxsynth/audio-plugins polluxsynth-audio-plugins
 cd polluxsynth-audio-plugins
 make -j 3
--- scripts/recipes/install_qmidiarp.sh
+++ scripts/recipes/install_qmidiarp.sh
@@ -5,16 +5,21 @@ if [ -d "qmidiarp" ]; then
 	rm -rf "qmidiarp"
 fi
 
+#wget https://github.com/emuse/qmidiarp/archive/refs/tags/qmidiarp-0.7.4.tar.gz
+#tar xfvz qmidiarp-0.7.4.tar.gz
+
 git clone --recursive https://github.com/emuse/qmidiarp.git
 #git clone https://github.com/zynthian/qmidiarp.git
 cd qmidiarp
 
 autoreconf -i
-sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" configure
-sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" configure.ac
+#sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" configure
+#sed -i -- "s/-msse -msse2 -mfpmath=sse/$CFLAGS $CFLAGS_UNSAFE/g" configure.ac
 ./configure --enable-lv2plugins
 make -j 3
 make install
 cd ..
 
 rm -rf "qmidiarp"
+
+#rm qmidiarp-0.7.4.tar.gz
