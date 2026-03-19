fc0300bbfd237a1f029f73a6ca86081be262a797
riban <riban@zynthian.org>
2026-03-19 06:18:14 +0000

    Bump patchlevel 20260318 to include V5 button fix.

--- scripts/update_zynthian_recipes.sh
+++ scripts/update_zynthian_recipes.sh
@@ -529,12 +529,11 @@ if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	aptpkgs="$aptpkgs python3-rarfile"
 fi
 
-patchlevel="20260318.3"
+patchlevel="20260318.4"
 if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	echo "Applying patch $patchlevel ..."
-	$ZYNTHIAN_RECIPE_DIR/fix_touch_envars.sh
+	fix_touch_envars.sh
 fi
-
 
 # -----------------------------------------------------------------------------
 # End of patches section
