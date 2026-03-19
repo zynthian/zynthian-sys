a9e78330134b05626a0b6c47ab3af5dadf571718
jofemodo <fernando@zynthian.org>
2026-03-19 16:10:34 +0100

    Force regeneration of boot & error screens to get the vangelis face.

--- scripts/update_zynthian_recipes.sh
+++ scripts/update_zynthian_recipes.sh
@@ -541,6 +541,12 @@ if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	$ZYNTHIAN_RECIPE_DIR/install_Ultramaster_KR-106_prebuilt.sh
 fi
 
+patchlevel="20260319.2"
+if [[ "$current_patchlevel" < "$patchlevel" ]]; then
+	echo "Applying patch $patchlevel ..."
+	rm -f /zynthian/config/img/*
+fi
+
 # -----------------------------------------------------------------------------
 # End of patches section
 # -----------------------------------------------------------------------------
