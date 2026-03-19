07ad05c5b04f4e5253054acb892c89c3a65db60b
jofemodo <fernando@zynthian.org>
2026-03-19 12:46:21 +0100

    Add new LV2 synth plugin "Ultramaster KR-106" (Juno106 emulator).

--- scripts/update_zynthian_recipes.sh
+++ scripts/update_zynthian_recipes.sh
@@ -535,6 +535,12 @@ if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	fix_touch_envars.sh
 fi
 
+patchlevel="20260319.1"
+if [[ "$current_patchlevel" < "$patchlevel" ]]; then
+	echo "Applying patch $patchlevel ..."
+	$ZYNTHIAN_RECIPE_DIR/install_Ultramaster_KR-106_prebuilt.sh
+fi
+
 # -----------------------------------------------------------------------------
 # End of patches section
 # -----------------------------------------------------------------------------
