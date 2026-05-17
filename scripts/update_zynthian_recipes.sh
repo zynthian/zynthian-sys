dda838d11e052e8c01debed3b3704f28f91516ef
riban <riban@zynthian.org>
2026-05-17 15:16:25 +0100

    Adds libsndfile1-dev

--- scripts/update_zynthian_recipes.sh
+++ scripts/update_zynthian_recipes.sh
@@ -629,12 +629,12 @@ if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	regenerate_lv2_presets.sh https://butoba.net/homepage/mimid.html
 fi
 
-patchlevel="20260517.1"
+patchlevel="20260517.2"
 if [[ "$current_patchlevel" < "$patchlevel" ]]; then
 	echo "Applying patch $patchlevel ..."
 	dpkg --remove --force-depends libsndfile-zyndev
 	apt --fix-broken install
-	aptpkgs="$aptpkgs libsndfile1"
+	aptpkgs="$aptpkgs libsndfile1 libsndfile1-dev"
 fi
 
 # -----------------------------------------------------------------------------
