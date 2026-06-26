6f69e5c4e400755cfac41dbe9ee5748eb42a81a3
jofemodo <fernando@zynthian.org>
2026-06-26 12:44:12 +0200

    Find the right "send_osc" path so it works in bookworm and trixie.

--- sbin/zynthian_stop.sh
+++ sbin/zynthian_stop.sh
@@ -2,7 +2,11 @@
 
 zynthian_pid=$(pgrep -f zynthian_main.py)
 if [[ "$zynthian_pid" ]]; then
-	send_osc 1370 /CUIA/EXIT_UI
+	if [[ -f "/zynthian/venv/bin/send_osc" ]]; then
+		/zynthian/venv/bin/send_osc 1370 /CUIA/EXIT_UI
+	else
+		send_osc 1370 /CUIA/EXIT_UI
+	fi
 	while [[ $(pgrep -f zynthian_main.py) == "$zynthian_pid" ]]; do
 		sleep 0.1
 	done
