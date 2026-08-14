635a1a68c90ac0b236918b22f6fa59e594daa598
jofemodo <fernando@zynthian.org>
2026-08-14 07:25:47 +0200

    Modify CLI script for launching UI with Wayland/X11.

--- sbin/zynthian.sh
+++ sbin/zynthian.sh
@@ -5,12 +5,21 @@ source "$ZYNTHIAN_SYS_DIR/scripts/zynthian_envars_extended.sh"
 export PYTHONFAULTHANDLER=1
 export ZYNTHIAN_LOG_LEVEL=10			# 10=DEBUG, 20=INFO, 30=WARNING, 40=ERROR, 50=CRITICAL
 
-if [ "$ZYNTHIAN_UI_ENABLE_CURSOR" == "1" ]; then
-	X11_SERVER_OPTIONS=""
-else
-	X11_SERVER_OPTIONS="-nocursor"
+if [[ "$1" == "wayland" ]]; then
+	export ZYNTHIAN_UI_WAYLAND="1"
 fi
 
 cd $ZYNTHIAN_UI_DIR
-startx ./zynthian.sh -- -r -s 0 $X11_SERVER_OPTIONS
-#startx ./zynthian.sh -- :0 vt3
+
+if [[ "$ZYNTHIAN_UI_WAYLAND" == "1" ]]; then
+	export PYOPENGL_PLATFORM=x11
+	labwc -S ./zynthian.sh
+else
+	if [ "$ZYNTHIAN_UI_ENABLE_CURSOR" == "1" ]; then
+		X11_SERVER_OPTIONS=""
+	else
+		X11_SERVER_OPTIONS="-nocursor"
+	fi
+	startx ./zynthian.sh -- -r -s 0 $X11_SERVER_OPTIONS
+	#startx ./zynthian.sh -- :0 vt3
+fi
